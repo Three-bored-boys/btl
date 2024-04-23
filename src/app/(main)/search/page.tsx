e125c8d06d4212015ae9d42faa1e1570b7878b81
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-04-23 23:30:12 +0100

    feat: update pages for nav-bar routes

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
 
 export default function Home(): ReactElement {
-  return <main className="text-3xl">Home</main>;
+  return <main className="text-3xl">Search</main>;
 }
