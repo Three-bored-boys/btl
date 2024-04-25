64c6051cf0d43cf71cdd470198c62ca5e416d36e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-04-25 13:23:48 +0100

    chore: update .env variables and add to .gitignore

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,8 @@
 declare namespace NodeJS {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface ProcessEnv {
     readonly GOOGLE_BOOKS_API_KEY: string;
     readonly NY_TIMES_BOOKS_API_KEY: string;
-    readonly NEXT_URL: string;
+    readonly NEXT_PUBLIC_URL: string;
   }
 }
