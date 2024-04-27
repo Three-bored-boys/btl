9d5bc4b031083ad7d7a431c33fa927fac6f3810f
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-04-27 06:43:53 +0100

    feat: create arrow-right icon component

---
+++ src/libs/client/src/components/ui/icons/arrow-right.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { cn } from "@/client/utils";
+
+type ArrowRightProps = React.ComponentProps<"svg">;
+
+export default function ArrowRight({ className, onClick, ...props }: ArrowRightProps): React.ReactElement {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className={cn("h-6 w-6 cursor-pointer", className)}
+      onClick={onClick}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  );
+}
