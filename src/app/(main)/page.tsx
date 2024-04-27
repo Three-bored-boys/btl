714813d6a33f3748af8eb452b70d3add8bba82b6
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-04-27 06:44:29 +0100

    feat: try hono RPC in a client component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,41 @@
+"use client";
+
 import Container from "@/libs/client/src/components/ui/container";
 import { ReactElement } from "react";
 import { client } from "@/libs/server/src/hono";
+import { useQuery } from "@tanstack/react-query";
 
-const getGenres = async function (): Promise<{
-  genres: {
-    name: string;
-    picture: string;
-  }[];
-  count: number;
-}> {
-  const res = await client.api.books.genres.$get();
-  const data = res.json();
+const getBooksByGenre = async function (genre: string) {
+  const res = await client.api.books.genres[":genre"].$get({ param: { genre } });
+  console.log(res);
+  if (!res.ok) {
+    const errorMessage = await res.text();
+    throw new Error(errorMessage);
+  }
+  const data = await res.json();
   return data;
 };
 
-export default async function HomePage(): Promise<ReactElement> {
-  const data = await getGenres();
+export default function HomePage(): ReactElement {
+  const genre = "Fiction";
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["genres", genre],
+    queryFn: async () => {
+      return await getBooksByGenre(genre);
+    },
+  });
+
+  if (isLoading) {
+    console.log(isLoading);
+    return <main>Loading...</main>;
+  }
+
+  if (error) {
+    console.log(error);
+    return <main>{error.message}</main>;
+  }
+
+  console.log(data);
   return (
     <main className="text-3xl">
       <Container>{JSON.stringify(data)}</Container>
