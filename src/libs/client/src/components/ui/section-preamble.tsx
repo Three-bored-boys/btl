399f5920fb85a9206434230d453e16c232a55a43
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-04-27 09:56:48 +0100

    feat: create SectionPreamble component

---
+++ src/libs/client/src/components/ui/section-preamble.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SectionPreamble({ title, children }: { title: string; children: React.ReactNode }): React.ReactElement {
+  return (
+    <>
+      <h1 className="text-2xl font-semibold tracking-wide sm:text-3xl">{title}</h1>
+      <p className="mb-4 text-xs sm:text-lg">{children}</p>
+    </>
+  );
+}
+
+export default SectionPreamble;
