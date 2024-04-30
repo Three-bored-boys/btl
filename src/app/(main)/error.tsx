65ec029a56c0b34b07b9b2f899ae7ba11f270887
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-04-30 23:17:29 +0100

    chore: edit (main) error page

--- src/app/(main)/error.tsx
+++ src/app/(main)/error.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import Container from "@/libs/client/src/components/layouts/container";
+
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   console.log(error.message);
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
-      <p>{error.message}</p>
-      <button onClick={() => reset()}>Try again</button>
+      <Container>
+        <h2>Something went wrong!</h2>
+        <p>{error.message}</p>
+        <button onClick={() => reset()}>Try again</button>
+      </Container>
     </div>
   );
 }
