ac504d3a4a56a235f78552597519822cc45d20da
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-04-30 15:02:08 +0100

    feat: change JSON response body types

--- src/libs/server/src/types/response.ts
+++ src/libs/server/src/types/response.ts
@@ -1,6 +1,6 @@
-export type JSONResponse<T = NonNullable<unknown>> =
-  | {
-      success: true;
-      data: T;
-    }
-  | { success: false; error: string };
+export type GoodResponse<T = NonNullable<unknown>> = {
+  success: true;
+  data: T;
+};
+
+export type BadResponse = { success: false; error: string };
