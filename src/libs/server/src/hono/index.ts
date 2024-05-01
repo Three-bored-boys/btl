1c2ca1cd974500938d6f8aac006f0d58b1ece92d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-01 08:34:13 +0100

    feat: uncouple chained hono routes

--- src/libs/server/src/hono/index.ts
+++ src/libs/server/src/hono/index.ts
@@ -1,20 +1,16 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-import { hc } from "hono/client";
 import books from "@/libs/server/src/hono/routes/books";
 
 const app = new Hono().basePath("/api");
 app.use(logger());
 app.use("*", cors());
 
-const routes = app
-  .get("/", (c) => {
-    return c.text("Welcome to the BTL API! 🚀");
-  })
-  .route("/books", books);
+app.get("/", (c) => {
+  return c.text("Welcome to the BTL API! 🚀");
+});
 
-export type AppType = typeof routes;
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_URL);
+app.route("/books", books);
 
 export default app;
--- src/libs/server/src/hono/routes/books.ts
+++ src/libs/server/src/hono/routes/books.ts
@@ -6,99 +6,104 @@ import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import type { BadResponse, GoodResponse, Genres, BestSeller, Book } from "../../types";
 
-const books = new Hono()
-  .get("/best-sellers", async (c) => {
-    const nytService = new NYTimesService(process.env.NY_TIMES_BOOKS_API_KEY);
-    const bestSellers = await nytService.getBestSellers();
+const books = new Hono();
 
-    if (bestSellers.length === 0) {
-      const responseData: BadResponse = { success: false, error: "Trouble getting NYT Best Sellers List" };
-      return c.json(responseData, 400);
-    }
+books.get("/best-sellers", async (c) => {
+  const nytService = new NYTimesService(process.env.NY_TIMES_BOOKS_API_KEY);
+  const bestSellers = await nytService.getBestSellers();
 
-    const responseData: GoodResponse<BestSeller[]> = { success: true, data: bestSellers };
-    return c.json(responseData);
-  })
-  .get("/genres", (c) => {
-    const responseData: GoodResponse<Genres> = { success: true, data: { genres, count: genres.length } };
-    return c.json(responseData);
-  })
-  .get(
-    "/genres/:genre",
-    zValidator(
-      "param",
-      z.object({
-        genre: z
-          .string()
-          .min(1)
-          .refine((val) => val !== "null" && val !== "undefined"),
-      }),
-      (result, c) => {
-        if (!result.success) {
-          const responseData: BadResponse = { success: false, error: "Invalid Input" };
-          return c.json(responseData, 404);
-        }
-      },
-    ),
-    async (c) => {
-      const { genre } = c.req.valid("param");
+  if (bestSellers.length === 0) {
+    const responseData: BadResponse = { success: false, error: "Trouble getting NYT Best Sellers List" };
+    return c.json(responseData, 400);
+  }
 
-      const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY);
-      const books = await googleBooksService.getBooksByGenre(genre);
+  const responseData: GoodResponse<BestSeller[]> = { success: true, data: bestSellers };
+  return c.json(responseData);
+});
 
-      const responseData: GoodResponse<Book[]> = { success: true, data: books };
+books.get("/genres", (c) => {
+  const responseData: GoodResponse<Genres> = { success: true, data: { genres, count: genres.length } };
+  return c.json(responseData);
+});
 
-      return c.json(responseData);
-    },
-  )
-  .get(
-    "/:isbn",
-    zValidator(
-      "param",
-      z.object({
-        isbn: z
-          .string()
-          .min(10)
-          .max(13)
-          .refine((val) => Number.isFinite(+val)),
-      }),
-      (result, c) => {
-        if (!result.success) {
-          const responseData: BadResponse = { success: false, error: "Invalid ISBN entry" };
-          return c.json(responseData, 400);
-        }
-      },
-    ),
-    async (c) => {
-      const isbn = c.req.param("isbn");
-      if (!isbn) {
-        const responseData: BadResponse = { success: false, error: "ISBN is required" };
-        return c.json(responseData, 400);
+books.get(
+  "/genres/:genre",
+  zValidator(
+    "param",
+    z.object({
+      genre: z
+        .string()
+        .min(1)
+        .refine((val) => val !== "null" && val !== "undefined"),
+    }),
+    (result, c) => {
+      if (!result.success) {
+        const responseData: BadResponse = { success: false, error: "Invalid Input" };
+        return c.json(responseData, 404);
       }
-
-      const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY);
-      const book = await googleBooksService.getBookByISBN(isbn);
-
-      if (!book) {
-        const responseData: BadResponse = { success: false, error: "Book not found" };
-        return c.json(responseData, 400);
-      }
-
-      const responseData: GoodResponse<Book> = { success: true, data: book };
-      return c.json(responseData);
     },
-  )
-  .get("/latest-books", async (c) => {
+  ),
+  async (c) => {
+    const { genre } = c.req.valid("param");
+
     const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY);
-    const books = await googleBooksService.getLatestBooks();
-
-    if (books.length === 0) {
-      const responseData: BadResponse = { success: false, error: "Trouble getting books" };
-      return c.json(responseData, 400);
-    }
+    const books = await googleBooksService.getBooksByGenre(genre);
 
     const responseData: GoodResponse<Book[]> = { success: true, data: books };
+
     return c.json(responseData);
-  });
+  },
+);
+
+books.get(
+  "/:isbn",
+  zValidator(
+    "param",
+    z.object({
+      isbn: z
+        .string()
+        .min(10)
+        .max(13)
+        .refine((val) => Number.isFinite(+val)),
+    }),
+    (result, c) => {
+      if (!result.success) {
+        const responseData: BadResponse = { success: false, error: "Invalid ISBN entry" };
+        return c.json(responseData, 400);
+      }
+    },
+  ),
+  async (c) => {
+    const isbn = c.req.param("isbn");
+    if (!isbn) {
+      const responseData: BadResponse = { success: false, error: "ISBN is required" };
+      return c.json(responseData, 400);
+    }
+
+    const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY);
+    const book = await googleBooksService.getBookByISBN(isbn);
+
+    if (!book) {
+      const responseData: BadResponse = { success: false, error: "Book not found" };
+      return c.json(responseData, 400);
+    }
+
+    const responseData: GoodResponse<Book> = { success: true, data: book };
+    return c.json(responseData);
+  },
+);
+
+books.get("/latest-books", async (c) => {
+  const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY);
+  const books = await googleBooksService.getLatestBooks();
+
+  if (books.length === 0) {
+    const responseData: BadResponse = { success: false, error: "Trouble getting books" };
+    return c.json(responseData, 400);
+  }
+
+  const responseData: GoodResponse<Book[]> = { success: true, data: books };
+  return c.json(responseData);
+});
 
 export default books;
