3a73a301c1b0a1fa94053f595c5b9ffae5a32725
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-02 21:32:26 +0100

    style: change font styles for SectionPreamble component

--- src/libs/client/src/components/modules/home/section-preamble.tsx
+++ src/libs/client/src/components/modules/home/section-preamble.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function SectionPreamble({ title, children }: { title: string; children: React.ReactNode }): React.ReactElement {
   return (
     <>
-      <h1 className="text-2xl font-semibold tracking-wide sm:text-3xl">{title}</h1>
+      <h1 className="text-2xl font-normal uppercase tracking-wide sm:text-3xl">{title}</h1>
       <p className="mb-4 text-xs sm:text-lg">{children}</p>
     </>
   );
