258a27f9a5ffb42b6d2e0fa08749a1c7927d2b69
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-02 07:58:23 +0100

    style: change style and structure of 'GenresSectionClient'

--- src/libs/client/src/components/modules/home/genres-section/genres-section-client.tsx
+++ src/libs/client/src/components/modules/home/genres-section/genres-section-client.tsx
@@ -25,21 +25,29 @@ export default function GenresSectionClient({ genres, count, ...props }: GenresS
   };
 
   return (
-    <div {...props} className="flex items-center justify-between">
-      <div className={cn({ "invisible cursor-default": genre === 0 })}>
-        <ArrowLeft onClick={prevGenre} />
-      </div>
+    <div className="w-full flex-col items-center">
+      <div {...props} className="w-full lg:grid lg:grid-cols-[auto_1fr_auto]">
+        <div className={cn({ "invisible cursor-default": genre === 0 }, "hidden lg:flex lg:items-center")}>
+          <ArrowLeft onClick={prevGenre} />
+        </div>
 
-      <div>
-        <h2 className="hover:text-brown-400 cursor-pointer border-b-4 border-transparent px-4 text-center text-xs uppercase lg:text-lg">
-          {genres[genre].name}
-        </h2>
-        <hr className="mb-5 h-1 w-full bg-primary" />
-        <GenreBooksShowcase heading={genres[genre].name} />
-      </div>
+        <div className="overflow-x-auto scrollbar-none">
+          <h2 className="cursor-pointer overflow-x-auto border-b-4 border-transparent px-4 text-center text-xs uppercase scrollbar-none lg:text-lg">
+            {genres[genre].name}
+          </h2>
+          <hr className="mb-5 h-1 w-full overflow-x-auto bg-primary scrollbar-none" />
+          <div className="flex w-full items-center justify-between gap-3 overflow-x-auto scrollbar-none">
+            <GenreBooksShowcase heading={genres[genre].name} />
+          </div>
+        </div>
 
-      <div className={cn({ "invisible cursor-default": genre === count - 1 })}>
-        <ArrowRight onClick={nextGenre} />
+        <div className={cn({ "invisible cursor-default": genre === count - 1 }, "hidden lg:flex lg:items-center")}>
+          <ArrowRight onClick={nextGenre} />
+        </div>
+      </div>
+      <div className={cn("flex w-full items-center justify-center lg:hidden")}>
+        <ArrowLeft onClick={prevGenre} className={cn({ "invisible cursor-default": genre === 0 })} />
+        <ArrowRight onClick={nextGenre} className={cn({ "invisible cursor-default": genre === count - 1 })} />
       </div>
     </div>
   );
