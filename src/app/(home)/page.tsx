0f911d8fc16aa83bb540b7e3c47d9898a69a1626
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-13 22:38:03 +0100

    chore: move home page to separate (home) directory

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,13 @@
 import GenresSection from "@/libs/client/src/components/modules/home/genres-section/genres-section";
+import Footer from "@/libs/client/src/components/layouts/footer/footer";
+import Header from "@/libs/client/src/components/modules/home/header/header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <GenresSection />
+      <Footer />
     </>
   );
 }
