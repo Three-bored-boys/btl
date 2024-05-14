9c6f55c77197f25fdbf8785f1fdd172fe0cf18a2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-14 22:00:29 +0100

    style: remove background color style for NavMenu component

--- src/libs/client/src/components/layouts/navigation-bar/nav-menu.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/nav-menu.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import { cn } from "@/client/utils";
 
-export default function NavMenu({
-  className,
-  children,
-  ...props
-}: React.ComponentProps<"div">): React.ReactElement {
+export default function NavMenu({ className, children, ...props }: React.ComponentProps<"div">): React.ReactElement {
   return (
-    <div
-      className={cn(
-        "absolute left-0 top-full w-full bg-brown-100 p-3 shadow-inner sm:p-4",
-        className,
-      )}
-      {...props}
-    >
+    <div className={cn("absolute left-0 top-full w-full p-3 shadow-inner sm:p-4", className)} {...props}>
       {children}
     </div>
   );
