292adf17ed669bcbff5e3fc7f9b1ce320d78b8eb
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-15 01:30:07 +0100

    feat: delete header component and rename hero-section directory

--- src/libs/client/src/components/modules/home/hero-section/hero-section.tsx
+++ src/libs/client/src/components/modules/home/hero-section/hero-section.tsx
@@ -2,7 +2,7 @@ import Container from "@/client/components/layouts/container";
 
 export default function HeroSection() {
   return (
-    <div>
+    <div className="w-full bg-primary pb-10 text-white">
       <Container>Yellow</Container>
     </div>
   );
