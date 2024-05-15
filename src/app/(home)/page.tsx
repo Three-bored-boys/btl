3ea0bb834e20bc9c3d70a0a70338d11f0eece11c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-15 01:01:24 +0100

    style: change root element in Home page to 'main' and add styles

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,15 @@
 import GenresSection from "@/libs/client/src/components/modules/home/genres-section/genres-section";
 import Footer from "@/libs/client/src/components/layouts/footer/footer";
 import Header from "@/libs/client/src/components/modules/home/header/header";
+import NavBar from "@/libs/client/src/components/layouts/navigation-bar/nav-bar";
 
 export default function Home() {
   return (
-    <>
+    <main className="relative">
+      <NavBar />
       <Header />
       <GenresSection />
       <Footer />
-    </>
+    </main>
   );
 }
