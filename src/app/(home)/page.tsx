51eb09f1b00f2256ea433d592237b733ad4566b0
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-15 01:35:18 +0100

    chore: replace Header with HeroSection

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,13 @@
 import GenresSection from "@/libs/client/src/components/modules/home/genres-section/genres-section";
 import Footer from "@/libs/client/src/components/layouts/footer/footer";
-import Header from "@/libs/client/src/components/modules/home/header/header";
 import NavBar from "@/libs/client/src/components/layouts/navigation-bar/nav-bar";
+import HeroSection from "@/libs/client/src/components/modules/home/hero-section/hero-section";
 
 export default function Home() {
   return (
     <main className="relative">
       <NavBar />
-      <Header />
+      <HeroSection />
       <GenresSection />
       <Footer />
     </main>
