dfd523f0665da6ce3f46d4ce0d29b9a7b6bef1ca
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-21 20:31:34 +0100

    chore: install radix ui themes package and add Theme at the root layout

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -2,9 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import { Theme,  } from "@radix-ui/themes";
 
 export default function Providers({ children }: { children: React.ReactNode }): React.ReactNode {
   const [queryClient] = React.useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Theme>{children}</Theme>
+    </QueryClientProvider>
+  );
 }
