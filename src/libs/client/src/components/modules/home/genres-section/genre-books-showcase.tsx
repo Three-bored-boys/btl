721c82c250f4710d76880930d54903eb44c61cdc
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-22 20:30:37 +0100

    style: make GenreBooksShowcase Loading Skeleton UI responsive

--- src/libs/client/src/components/modules/home/genres-section/genre-books-showcase.tsx
+++ src/libs/client/src/components/modules/home/genres-section/genre-books-showcase.tsx
@@ -35,12 +35,12 @@ export default function GenreBooksShowcase({ heading }: GenreBooksShowcaseProps)
             <Skeleton key={i} className="block h-full w-full rounded-lg"></Skeleton>
           ))}
         </div>
-        <div className="grid h-56 w-36 grid-cols-1 grid-rows-[12rem_1fr_1fr] gap-y-2 sm:h-72 sm:grid-rows-[15rem_1fr_1fr]">
+        <div className="hidden h-56 w-36 grid-cols-1 grid-rows-[12rem_1fr_1fr] gap-y-2 xs:grid sm:h-72 sm:grid-rows-[15rem_1fr_1fr]">
           {Array.from({ length: 3 }, (_, i) => i).map((_, i) => (
             <Skeleton key={i} className="block h-full w-full rounded-lg"></Skeleton>
           ))}
         </div>
-        <div className="grid h-56 w-36 grid-cols-1 grid-rows-[12rem_1fr_1fr] gap-y-2 sm:h-72 sm:grid-rows-[15rem_1fr_1fr]">
+        <div className="hidden h-56 w-36 grid-cols-1 grid-rows-[12rem_1fr_1fr] gap-y-2 sm:grid sm:h-72 sm:grid-rows-[15rem_1fr_1fr]">
           {Array.from({ length: 3 }, (_, i) => i).map((_, i) => (
             <Skeleton key={i} className="block h-full w-full rounded-lg"></Skeleton>
           ))}
