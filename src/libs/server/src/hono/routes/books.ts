61a932b8876adb59fd3ada007b72b7592ca3892d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-24 21:53:24 +0100

    chore: edit Cache-Control header value

--- src/libs/server/src/hono/routes/books.ts
+++ src/libs/server/src/hono/routes/books.ts
@@ -18,7 +18,7 @@ books.get("/best-sellers", async (c) => {
   }
 
   const responseData: GoodResponse<BestSeller[]> = { success: true, data: bestSellers };
-  c.header("Cache-Control", "max-age=259200, must-revalidate");
+  c.header("Cache-Control", "max-age=259200, must-revalidate, public");
   return c.json(responseData);
 });
 
