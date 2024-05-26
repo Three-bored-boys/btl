c10cc055fb9d47704d63c8eab17ff08d605990a3
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-26 01:26:30 +0100

    chore: update .env variables

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,9 @@
 declare namespace NodeJS {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface ProcessEnv {
-    readonly GOOGLE_BOOKS_API_KEY: string;
-    readonly NY_TIMES_BOOKS_API_KEY: string;
-    readonly NEXT_PUBLIC_URL: string;
-    readonly NEXT_PUBLIC_API_URL: string;
+    readonly API_URL_PROD: string;
+    readonly NEXT_PUBLIC_API_URL_PROD: string;
+    readonly API_URL_DEV: string;
+    readonly NEXT_PUBLIC_API_URL_DEV: string;
   }
 }
