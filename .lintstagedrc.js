16a66e7796cbca6ec79412e79b438e537ecbccc2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-30 23:20:22 +0100

    build: update lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,5 +4,6 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
 
 module.exports = {
+  "!(*.{js,jsx,ts,tsx})": ["prettier --write"],
   "*.{js,jsx,ts,tsx}": ["prettier --write", buildEslintCommand],
 };
