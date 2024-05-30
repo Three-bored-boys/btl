f98cef6023964586f05df67e1eabcc9a1e95423f
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-05-30 23:20:58 +0100

    chore: remove console.log

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,7 +7,6 @@ import NYTBestSellersSection from "@/libs/client/src/components/modules/home/nyt
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  console.log("testing lint-staged");
   return (
     <main className="relative">
       <NavBar />
