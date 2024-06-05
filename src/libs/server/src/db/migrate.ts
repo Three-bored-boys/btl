96be72f549538ef708623768de03974d480b55e8
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-06-05 23:36:28 +0100

    chore: move db initialisations to index.ts

--- src/libs/server/src/db/migrate.ts
+++ src/libs/server/src/db/migrate.ts
@@ -1,16 +1,9 @@
-import { config } from "dotenv";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
-
-config({ path: ".dev.vars" });
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const databaseUrl = drizzle(postgres(process.env.DATABASE_URL!, { max: 1 }));
+import { migrationClient } from "./index";
 
 const main = async () => {
   try {
-    await migrate(databaseUrl, { migrationsFolder: "drizzle" });
+    await migrate(migrationClient, { migrationsFolder: "drizzle" });
     console.log("Migration complete");
   } catch (error) {
     console.log(error);
