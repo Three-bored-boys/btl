a36c54502442fb17f5578d12da66a73e828b233e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-06-05 02:21:15 +0100

    chore: remove commented code

--- src/libs/server/src/db/migrate.ts
+++ src/libs/server/src/db/migrate.ts
@@ -2,7 +2,6 @@ import { config } from "dotenv";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-// import path from "path";
 
 config({ path: ".dev.vars" });
 
