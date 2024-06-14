e3721adf3b39a5b903b8d02b0533e7379341cd4e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-06-14 23:53:05 +0100

    style: add styles to Quote component

--- src/libs/client/src/components/modules/home/quotes-section/quote.tsx
+++ src/libs/client/src/components/modules/home/quotes-section/quote.tsx
@@ -6,10 +6,10 @@ type QuoteProps = { author: string; quote: string } & ComponentProps<"div">;
 export default function Quote({ author, quote, className, ...props }: QuoteProps) {
   return (
     <div className={cn("inline-block w-full", className)} {...props}>
-      <div>
-        <q className="inline-block">{quote}</q>
+      <div className="mb-5 text-wrap text-center font-medium">
+        <q className="inline-block w-full text-base italic xs:text-lg sm:text-xl md:text-2xl lg:text-3xl">{quote}</q>
       </div>
-      <div>{author}</div>
+      <div className="text-center text-sm font-normal sm:text-base md:text-lg lg:text-xl">- {author}</div>
     </div>
   );
 }
