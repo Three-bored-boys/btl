1bd6c314f12d6aee5eec4ec09ed3b24f9f502b3f
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-06-16 22:57:36 +0100

    chore: update GenresSectionInnerBooksShowcase component

--- src/libs/client/src/components/modules/home/genres-section/genres-section-inner-books-showcase.tsx
+++ src/libs/client/src/components/modules/home/genres-section/genres-section-inner-books-showcase.tsx
@@ -1,7 +1,6 @@
-import type { Book } from "@/libs/server/src/types";
+// import type { Book } from "@/libs/server/src/types";
 import BookCard from "../book-card";
-import { useQuery } from "@tanstack/react-query";
-import { fetchData } from "@/libs/client/src/hooks";
+import { useGenresSectionBooksInnerBooksShowcase } from "@/libs/client/src/hooks";
 import { Skeleton } from "@radix-ui/themes";
 
 type GenresSectionInnerBooksShowcaseProps = {
@@ -9,15 +8,10 @@ type GenresSectionInnerBooksShowcaseProps = {
 };
 
 export default function GenresSectionInnerBooksShowcase({ heading }: GenresSectionInnerBooksShowcaseProps) {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["genres", heading],
-    queryFn: async () => {
-      const data = await fetchData<Book[]>(`${process.env.NEXT_PUBLIC_API_URL}/books/genres/${heading}`);
-      return data;
-    },
-  });
+  const { data, isLoading, error } = useGenresSectionBooksInnerBooksShowcase(heading);
 
   if (isLoading) {
+    console.log("Loading...");
     return (
       <div className="flex w-full items-center justify-between gap-3 overflow-x-auto scrollbar-thin">
         <div className="grid h-56 w-36 grid-cols-1 grid-rows-[12rem_1fr_1fr] gap-y-2 sm:h-72 sm:grid-rows-[15rem_1fr_1fr]">
@@ -55,9 +49,11 @@ export default function GenresSectionInnerBooksShowcase({ heading }: GenresSecti
   }
 
   if (error) {
+    console.log("There is an error");
     return <div className="h-72 w-full text-center text-lg font-medium">Error has occurred: {error.message}</div>;
   }
 
+  console.log("No loading or error");
   return (
     <div className="flex w-full items-center justify-between gap-3 overflow-x-auto scrollbar-thin">
       {data?.map((book, i) => <BookCard key={i} book={book} />)}
