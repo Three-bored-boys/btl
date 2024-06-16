b074db1f0357a8fe6a21824293356cbb807d891c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-06-16 11:26:02 +0100

    style: remove w-full class from Quote component

--- src/libs/client/src/components/modules/home/quotes-section/quote.tsx
+++ src/libs/client/src/components/modules/home/quotes-section/quote.tsx
@@ -5,7 +5,7 @@ type QuoteProps = { author: string; quote: string } & ComponentProps<"div">;
 
 export default function Quote({ author, quote, className, ...props }: QuoteProps) {
   return (
-    <div className={cn("inline-block w-full", className)} {...props}>
+    <div className={cn("inline-block", className)} {...props}>
       <div className="mb-5 text-wrap text-center font-medium">
         <q className="inline-block w-full text-base italic xs:text-lg sm:text-xl md:text-2xl lg:text-3xl">{quote}</q>
       </div>
