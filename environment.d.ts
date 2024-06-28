a887737e931f7bee20068ee0286411beb41f9f6c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-06-28 19:53:37 +0100

    fix: error on home page about error in rendering server component

--- environment.d.ts
+++ environment.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface ProcessEnv {
     readonly NEXT_PUBLIC_API_URL: string;
+    readonly API_URL: string;
   }
 }
