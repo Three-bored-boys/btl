f53c45465dd643273f4358ec4774fb5852529488
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-06-30 22:19:40 +0100

    style: change the auth layout page images to background images

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import Logo from "../../libs/client/src/components/ui/logo";
 import { cn } from "../../libs/client/src/utils";
-import authLayoutImage1 from "@/public/assets/images/authPage/authLayout1.jpg";
-import authLayoutImage2 from "@/public/assets/images/authPage/authLayout2.jpg";
-import authLayoutImage3 from "@/public/assets/images/authPage/authLayout3.jpg";
-import authLayoutImage4 from "@/public/assets/images/authPage/authLayout4.jpg";
-import authLayoutImage5 from "@/public/assets/images/authPage/authLayout5.jpg";
 
 export default function AuthLayout({
   children,
@@ -16,30 +10,22 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>): React.ReactElement {
   const authLayoutImagesArray = [
-    authLayoutImage1,
-    authLayoutImage2,
-    authLayoutImage3,
-    authLayoutImage4,
-    authLayoutImage5,
+    "bg-auth-image-1",
+    "bg-auth-image-2",
+    "bg-auth-image-3",
+    "bg-auth-image-4",
+    "bg-auth-image-5",
   ];
+
   const authLayoutImageIndex = React.useRef(Math.floor(Math.random() * authLayoutImagesArray.length));
-  const layoutBackground = authLayoutImagesArray[authLayoutImageIndex.current];
-  console.log(authLayoutImageIndex);
+  const bgImageClassName = authLayoutImagesArray[authLayoutImageIndex.current];
 
   return (
     <main className="grid w-full grid-cols-2 grid-rows-[100vh]">
-      <div className="relative overflow-hidden">
+      <div className={cn("relative bg-cover bg-center", bgImageClassName)}>
         <Logo
           className={cn("absolute left-5 top-5 text-5xl", { "text-white": authLayoutImageIndex.current === 0 })}
         ></Logo>
-        <Image
-          src={layoutBackground}
-          alt="Image about books"
-          width={1000}
-          height={1000}
-          className="h-full w-full object-cover"
-          placeholder="blur"
-        ></Image>
       </div>
       <div className="overflow-y-auto px-40 py-20">{children}</div>
     </main>
