29e169d452150ac9ed40bfaba5d16d151c004dca
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-07-16 23:06:23 +0100

    chore: minor edit to book page

--- src/app/(main)/book/[isbn]/page.tsx
+++ src/app/(main)/book/[isbn]/page.tsx
@@ -1,10 +1,16 @@
 import Container from "@/libs/client/src/components/layouts/container";
 import { ReactElement } from "react";
 
-export default function Book(): ReactElement {
+export default function Book({
+  params: { isbn },
+  searchParams,
+}: {
+  params: { isbn: string };
+  searchParams: Record<string, string | string[] | undefined>;
+}): ReactElement {
   return (
     <main className="text-3xl">
-      <Container>Book Page</Container>
+      <Container>Book Page: {isbn}</Container>
     </main>
   );
 }
