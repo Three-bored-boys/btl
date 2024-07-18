1aa61e0eec867c39fa98b1f224c5cdd87262cb3a
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-07-18 22:28:48 +0100

    feat: create BookInformation component

---
+++ src/libs/client/src/components/modules/book-page/book-information.tsx
@@ -0,0 +1,31 @@
+import Container from "@/libs/client/src/components/layouts/container";
+import type { Book } from "@/root/src/libs/server/src/types";
+import { ComponentProps } from "react";
+import Image from "next/image";
+
+const genericBookImage = "/assets/images/generic-book.png";
+
+type BookInformationProps = { book: Book } & ComponentProps<"div">;
+
+export default function BookInformation({ book, ...props }: BookInformationProps) {
+  return (
+    <div {...props}>
+      <Container>
+        Book Page: {book.isbn13} <br />
+        `&quot;`, `&ldquo;`, `&#34;`, `&rdquo;` <br />
+        {JSON.stringify(book)}
+        <div className="w-32 min-w-28 sm:w-40 sm:min-w-36" {...props}>
+          <Image
+            src={book.image ? book.image : genericBookImage}
+            alt={`${book.title} by ${book.author}`}
+            className="h-44 w-full rounded-lg object-cover sm:h-60"
+            width={500}
+            height={500}
+          />
+          <h4 className="truncate font-semibold">{book.title}</h4>
+          <p className="truncate">{book.author}</p>
+        </div>
+      </Container>
+    </div>
+  );
+}
