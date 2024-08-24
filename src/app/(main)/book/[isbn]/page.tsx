7ed29c204259e161c69d3f26a7d3a89f58ba9b4b
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-08-24 10:24:40 +0100

    chore: change name of returned JSX to BookPage

--- src/app/(main)/book/[isbn]/page.tsx
+++ src/app/(main)/book/[isbn]/page.tsx
@@ -1,9 +1,9 @@
-import BookInformation from "@/root/src/libs/client/src/components/modules/book-page/book-information";
+import BookPage from "@/root/src/libs/client/src/components/modules/book-page/book-page";
 import { fetchData } from "@/root/src/libs/client/src/hooks";
 import type { Book } from "@/root/src/libs/server/src/types";
 
 export default async function Book({ params: { isbn } }: { params: { isbn: string } }) {
   const bookObject = await fetchData<Book>(`${process.env.API_URL}/books/${isbn}`);
 
-  return <BookInformation book={bookObject} />;
+  return <BookPage book={bookObject} />;
 }
