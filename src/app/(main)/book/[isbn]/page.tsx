63e431a547779a5ae14b76d1e069bed5d02ff94d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-08-25 22:59:53 +0100

    feat: render a different UI for when no book is found by ISBN search

--- src/app/(main)/book/[isbn]/page.tsx
+++ src/app/(main)/book/[isbn]/page.tsx
@@ -3,7 +3,11 @@ import { fetchData } from "@/root/src/libs/client/src/hooks";
 import type { Book } from "@/root/src/libs/server/src/types";
 
 export default async function Book({ params: { isbn } }: { params: { isbn: string } }) {
-  const bookObject = await fetchData<Book>(`${process.env.API_URL}/books/${isbn}`);
+  const bookObject = await fetchData<Book | string>(`${process.env.API_URL}/books/${isbn}`);
+
+  if (typeof bookObject === "string") {
+    return <div>{bookObject}</div>;
+  }
 
   return <BookPage book={bookObject} />;
 }
