c2d328b40eda1ffe98e016bd053d6c7dec38644a
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-09-01 21:36:38 +0100

    chore: set up radio group for adding/removing book from library

--- src/libs/client/src/components/modules/book-page/add-to-library.tsx
+++ src/libs/client/src/components/modules/book-page/add-to-library.tsx
@@ -2,24 +2,24 @@ import BookOpen from "@/client/components/ui/icons/book-open";
 import ListBullet from "@/client/components/ui/icons/list-bullet";
 import Check from "@/client/components/ui/icons/check";
 import Trash from "@/client/components/ui/icons/trash";
-import NoSymbol from "@/client/components/ui/icons/no-symbol";
+import { RadioCards } from "@radix-ui/themes";
 
 const bookLocations = [
   { name: "Currently Reading", value: "reading", icon: <BookOpen /> },
   { name: "Want To Read", value: "want-to-read", icon: <ListBullet /> },
   { name: "Finished", value: "finished", icon: <Check /> },
   { name: "Did Not Finish", value: "did-not-finish", icon: <Trash /> },
-  { name: "Not In Library", value: "not-in-library", icon: <NoSymbol></NoSymbol> },
-];
+] as const;
 
 export default function AddToLibrary() {
   return (
-    <select name="location" id="location">
+    <RadioCards.Root>
       {bookLocations.map((obj, i) => (
-        <option value={obj.value} key={i} disabled={obj.value === "not-in-library"}>
-          {obj.name}
-        </option>
+        <RadioCards.Item value={obj.value} key={i} checked={obj.value === "did-not-finish"}>
+          <p>{obj.name}</p>
+          {obj.icon}
+        </RadioCards.Item>
       ))}
-    </select>
+    </RadioCards.Root>
   );
 }
