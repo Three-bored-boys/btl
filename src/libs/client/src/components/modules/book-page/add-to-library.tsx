688d92235d9b863a35beb97d5e75a1d85b4f5ce3
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-09-03 21:48:05 +0100

    feat: add input radio buttons to add or remove a book from a user's library

--- src/libs/client/src/components/modules/book-page/add-to-library.tsx
+++ src/libs/client/src/components/modules/book-page/add-to-library.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import BookOpen from "@/client/components/ui/icons/book-open";
 import ListBullet from "@/client/components/ui/icons/list-bullet";
 import Check from "@/client/components/ui/icons/check";
 import Trash from "@/client/components/ui/icons/trash";
 import { RadioCards } from "@radix-ui/themes";
+import Button from "../../ui/button";
+import { cn } from "../../../utils";
+import { useState } from "react";
 
 const bookLocations = [
   { name: "Currently Reading", value: "reading", icon: <BookOpen /> },
@@ -12,14 +17,33 @@ const bookLocations = [
 ] as const;
 
 export default function AddToLibrary() {
+  const [location, setLocation] = useState<string | null>(null);
   return (
-    <RadioCards.Root>
-      {bookLocations.map((obj, i) => (
-        <RadioCards.Item value={obj.value} key={i} checked={obj.value === "did-not-finish"}>
-          <p>{obj.name}</p>
-          {obj.icon}
-        </RadioCards.Item>
-      ))}
-    </RadioCards.Root>
+    <div className="grid grid-cols-[1fr_20rem] gap-4">
+      <div>
+        <RadioCards.Root size={"1"} color="bronze">
+          {bookLocations.map((obj, i) => (
+            <RadioCards.Item
+              value={obj.value}
+              key={i}
+              className={cn("hover:bg-secondary-300")}
+              title={`Add to '${obj.name}'`}
+              checked={obj.value === location}
+              onClick={() => setLocation(obj.value)}
+            >
+              <span>{obj.name}</span>
+              {obj.icon}
+            </RadioCards.Item>
+          ))}
+        </RadioCards.Root>
+      </div>
+      <div className="flex items-center justify-start">
+        {location && (
+          <Button background={"dark"} className="text-[10px]" textSize={"small"} onClick={() => setLocation(null)}>
+            Remove from Library
+          </Button>
+        )}
+      </div>
+    </div>
   );
 }
