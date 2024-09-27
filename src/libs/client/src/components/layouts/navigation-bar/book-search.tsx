ffe05e24753175d073a1b14bfac84bdc975f9dfa
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-09-27 19:24:14 +0100

    chore: add more functionality to BookSearch component

--- src/libs/client/src/components/layouts/navigation-bar/book-search.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/book-search.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { ComponentProps, useState } from "react";
 import MagnifyingGlass from "../../ui/icons/magnifying-glass";
+import { cn } from "../../../utils";
 
-export default function BookSearch() {
+export default function BookSearch({ className }: ComponentProps<"div">) {
+  const [searchInput, setSearchInput] = useState<string>("");
+
   return (
-    <div>
-      <div className="flex items-center justify-start gap-1">
+    <div className={cn(className)}>
+      <div className="flex w-full items-center justify-start gap-1">
         <MagnifyingGlass />
         <input
           type="search"
-          className="w-full rounded-lg border-2 px-1 text-base outline-none sm:w-4/5 md:w-full lg:text-lg"
+          className="w-full rounded-lg border-2 border-primary-50 px-1 text-base outline-0 hover:border-primary-100 focus:border-primary sm:w-full lg:text-lg"
           placeholder="Enter a book or string..."
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+            console.log(e.target.value);
+          }}
         />
       </div>
     </div>
