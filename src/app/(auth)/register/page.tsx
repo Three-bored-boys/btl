deec66b2dee34411b5c36524e05bcf3b85c87abb
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-03 19:30:48 +0100

    fix: error about React being a UMD global

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import RegisterForm from "@/client/components/modules/auth/register/register-form";
 import Link from "next/link";
+import React from "react";
 
 export default function Register(): ReactElement {
   return (
