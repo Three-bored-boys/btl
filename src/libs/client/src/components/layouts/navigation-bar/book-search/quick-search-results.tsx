c5a9717ae30dc0267679716b78b8ef1d6ccde4d2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-03 19:57:18 +0100

    feat: create QuickSearchResults component file

---
+++ src/libs/client/src/components/layouts/navigation-bar/book-search/quick-search-results.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import { useQuery } from "@tanstack/react-query";
+
+export default function QuickSearchResults({ search }: { search: string }) {
+  return <div>{search}</div>;
+}
