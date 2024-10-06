de0c29640d833aa084ff8bbf541d2f6f80de5620
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-06 19:23:37 +0100

    chore: render QuickSearchResults loading skeleton

--- src/libs/client/src/components/layouts/navigation-bar/book-search/quick-search-results.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/book-search/quick-search-results.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React from "react";
+// import { book } from "./data";
+import LoadingSkeleton from "./loading-skeleton";
 import useQuickSearchResults from "./useQuickSearchResults";
 
 export default function QuickSearchResults({ search }: { search: string }) {
   const { isFetching, error, data } = useQuickSearchResults({ search });
 
-  if (isFetching) return <div>Loading...</div>;
+  if (isFetching) return <LoadingSkeleton />;
 
   if (error) return <div>Error!!!</div>;
 
