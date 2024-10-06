bd307c6dbf79d6e04b33ceadd58a82629272e41a
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-06 19:11:48 +0100

    feat: create loading skeleton for QuickSearchResults

---
+++ src/libs/client/src/components/layouts/navigation-bar/book-search/loading-skeleton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Skeleton } from "@radix-ui/themes";
+
+export default function LoadingSkeleton() {
+  return (
+    <div className="grid w-full grid-cols-[40px_1fr] grid-rows-[auto] gap-1 xs:grid-cols-[60px_1fr] max-lg:md:grid-cols-[50px_1fr]">
+      <div className="aspect-square">
+        <Skeleton className="block h-full w-full rounded-xl"></Skeleton>
+      </div>
+      <div className="grid grid-cols-1 grid-rows-[1fr_1fr] gap-[40%]">
+        <div className="w-full">
+          <Skeleton className="block h-full w-full rounded-xl"></Skeleton>
+        </div>
+        <div className="w-full">
+          <Skeleton className="block h-full w-full rounded-xl"></Skeleton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/* xs:grid-cols-[55px_1fr] sm:grid-cols-[70px_1fr]  --- lg:grid-cols-[1fr_7fr]*/
