17a1e099c2d134bca65ea2a15c0ee205d7e16121
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-07 18:37:34 +0100

    style: create draft look for books from quick search results

--- src/libs/client/src/components/layouts/navigation-bar/book-search/quick-search-results.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/book-search/quick-search-results.tsx
@@ -1,16 +1,65 @@
 "use client";
 
 import React from "react";
-// import { book } from "./data";
-import LoadingSkeleton from "./loading-skeleton";
-import useQuickSearchResults from "./useQuickSearchResults";
+import { book } from "./data";
+import Image from "next/image";
+import Link from "next/link";
+// import LoadingSkeleton from "./loading-skeleton";
+// import useQuickSearchResults from "./useQuickSearchResults";
+// import type { Book } from "@/root/src/libs/server/src/types";
 
 export default function QuickSearchResults({ search }: { search: string }) {
-  const { isFetching, error, data } = useQuickSearchResults({ search });
+  // const { isFetching, error, data } = useQuickSearchResults({ search });
 
-  if (isFetching) return <LoadingSkeleton />;
+  // if (isFetching) return <LoadingSkeleton />;
 
-  if (error) return <div>Error!!!</div>;
+  // if (error) return <div>Error!!!</div>;
 
-  return <div>{JSON.stringify(data)}</div>;
+  return Array.from({ length: 3 }, (_, i) => i).map((_, i) => (
+    <Link
+      className="mb-3 grid w-full grid-cols-[40px_1fr] grid-rows-[auto] gap-1 xs:grid-cols-[45px_1fr] max-lg:md:grid-cols-[40px_1fr]"
+      href={`/book/${book.isbn13}`}
+      key={i}
+    >
+      <div className="aspect-square">
+        <Image
+          width={500}
+          height={500}
+          src={book.image}
+          alt={`${book.title} by ${book.author}`}
+          className="mx-auto h-full w-full rounded-lg object-cover"
+        />
+      </div>
+      <div className="truncate text-sm">
+        <p className="mb-1 truncate font-medium leading-4">{book.title}</p>
+        <p className="truncate font-extralight leading-4">{book.author}</p>
+      </div>
+    </Link>
+  ));
+
+  /* return (
+    <>
+      {data?.map((book, i) => (
+        <Link
+          className="grid w-full grid-cols-[40px_1fr] grid-rows-[auto] gap-1 xs:grid-cols-[45px_1fr] max-lg:md:grid-cols-[40px_1fr]"
+          href={`/book/${book.isbn13}`}
+          key={i}
+        >
+          <div className="aspect-square">
+            <Image
+              width={500}
+              height={500}
+              src={book.image}
+              alt={`${book.title} by ${book.author}`}
+              className="mx-auto h-full w-full rounded-lg object-cover"
+            />
+          </div>
+          <div className="truncate text-sm">
+            <p className="mb-1 truncate font-medium leading-4">{book.title}</p>
+            <p className="truncate font-extralight leading-4">{book.author}</p>
+          </div>
+        </Link>
+      ))}
+    </>
+  ); */
 }
