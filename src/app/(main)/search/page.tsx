21aa1a3bb7d5b11cab41409a7e56224fdd73f0dd
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-04 23:23:36 +0000

    chore: edit search route page

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,13 +1,10 @@
-import Container from "@/libs/client/src/components/layouts/container";
-import SearchBar from "@/client/components/modules/search-bar/search-bar";
 import { ReactElement } from "react";
+import AdjustmentsHorizontal from "@/client/components/ui/icons/adjustments-horizontal";
 
 export default function Search(): ReactElement {
   return (
-    <div className="text-3xl">
-      <Container>
-        <SearchBar></SearchBar>
-      </Container>
+    <div>
+      <AdjustmentsHorizontal />
     </div>
   );
 }
