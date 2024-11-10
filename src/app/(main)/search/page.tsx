a137ded7438b836f072fe05b53bfb679afc9e712
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-10 10:35:23 +0000

    fix: try again

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from "react";
 import { SearchPage } from "@/root/src/libs/client/src/components/modules/search-page/search-page";
 
+export const revalidate = 0;
+
 export default function Search(): ReactElement {
   return <SearchPage />;
 }
