cf6694e28136c091ad25a90ecf7a49c2fb0eac5c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-20 22:54:31 +0000

    test: another one

--- src/libs/client/src/components/layouts/search-page-layout/hooks.ts
+++ src/libs/client/src/components/layouts/search-page-layout/hooks.ts
@@ -86,7 +86,6 @@ export function useSearchPage(): SearchPageHookReturnType {
     }).toString();
 
     if (searchParams.toString() !== updatedParamsObject) router.replace(`/search?${updatedParamsObject}`);
-    /* eslint-disable-next-line */ // I only need this to run once on mount
   }, []);
 
   return [filters, allInputElementRefsMap, searchInputElement, searchObjectRef, searchParams, router];
