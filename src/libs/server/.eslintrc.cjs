ebdf4cbef8c11eead08068de5b5166c5f1c74be9
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-24 14:32:53 +0000

    chore: update eslintrc for server lib

--- src/libs/server/.eslintrc.cjs
+++ src/libs/server/.eslintrc.cjs
@@ -1,8 +1,22 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@hono/eslint-config"],
-  rules: {
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
+  "root": true,
+  "extends": ["plugin:@typescript-eslint/strict-type-checked", "plugin:@typescript-eslint/stylistic-type-checked"],
+  "plugins": ["@typescript-eslint"],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "./tsconfig.json",
+  },
+  "rules": {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
+    "@typescript-eslint/no-confusing-void-expression": [
+      "error",
+      {
+        "ignoreArrowShorthand": true,
+      },
+    ],
+    "@typescript-eslint/no-unnecessary-condition": "off",
   },
 };
