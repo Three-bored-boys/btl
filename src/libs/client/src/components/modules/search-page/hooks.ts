b89cf6578d7c9d0dabc2e2bf32cadf6a8c20d129
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-25 20:06:54 +0000

    refactor: useSearchPage custom hook

--- src/libs/client/src/components/modules/search-page/hooks.ts
+++ src/libs/client/src/components/modules/search-page/hooks.ts
@@ -1,15 +1,10 @@
 import { PaginationObjectType, SearchObjectType } from "@/root/src/libs/shared/src/schemas";
-import {
-  editSearchObjectInLocalStorage,
-  fetchData,
-  getSearchObjectFromLocalStorage,
-  handleNumberSearchParam,
-} from "../../../utils";
+import { fetchData, getSearchObjectFromLocalStorage, handleNumberSearchParam } from "../../../utils";
 import { DEFAULT_MAX_RESULTS, DEFAULT_START_INDEX, filterKeysArray } from "@/libs/shared/src/utils";
 import { Book } from "@/root/src/libs/shared/src/types";
 import { useQuery } from "@tanstack/react-query";
 import { ReadonlyURLSearchParams, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 const getFullSearchResults = async function (searchObject: SearchObjectType, paginationObject: PaginationObjectType) {
@@ -39,14 +34,8 @@ type SearchPageHookReturnType = {
   filters: React.MutableRefObject<(keyof SearchObjectType)[]>;
   allInputElementRefsMap: React.MutableRefObject<Map<string, HTMLInputElement | null>>;
   searchInputElement: React.MutableRefObject<HTMLInputElement | null>;
-  searchObjectRef: React.MutableRefObject<SearchObjectType>;
-  paginationObjectRef: React.MutableRefObject<PaginationObjectType>;
   searchParams: ReadonlyURLSearchParams;
   router: AppRouterInstance;
-  paginationObjectState: PaginationObjectType;
-  setPaginationObjectState: React.Dispatch<React.SetStateAction<PaginationObjectType>>;
-  searchObjectState: SearchObjectType;
-  setSearchObjectState: React.Dispatch<React.SetStateAction<SearchObjectType>>;
 };
 
 export function useSearchPage(): SearchPageHookReturnType {
@@ -54,34 +43,22 @@ export function useSearchPage(): SearchPageHookReturnType {
   const allInputElementRefsMap = useRef<Map<keyof SearchObjectType, HTMLInputElement | null>>(
     new Map(filters.current.map((str) => [str, null])),
   );
-
-  const searchObjectRef = useRef<SearchObjectType>({});
-  const paginationObjectRef = useRef<PaginationObjectType>({});
-  const [searchObjectState, setSearchObjectState] = useState<SearchObjectType>({});
-  const [paginationObjectState, setPaginationObjectState] = useState<PaginationObjectType>({});
   const searchInputElement = useRef<HTMLInputElement | null>(null);
 
   const searchParams = useSearchParams();
   const router = useRouter();
 
   useEffect(() => {
-    if (window) {
-      searchObjectRef.current = getSearchObjectFromLocalStorage();
-    }
-
     const searchParamsObject = new URLSearchParams(searchParams.toString());
 
     const searchQueryParam = searchParamsObject.get("search");
     if (searchInputElement.current !== null && searchQueryParam !== null) {
       searchInputElement.current.value = searchQueryParam;
-      searchObjectRef.current = editSearchObjectInLocalStorage("search", searchQueryParam);
-    } else if (
-      searchInputElement.current !== null &&
-      searchQueryParam === null &&
-      searchObjectRef.current.search !== undefined
-    ) {
-      searchInputElement.current.value = searchObjectRef.current.search;
-      searchParamsObject.set("search", searchObjectRef.current.search);
+    } else if (searchInputElement.current !== null && searchQueryParam === null && window) {
+      const searchObject = getSearchObjectFromLocalStorage();
+      if (searchObject.search !== undefined) {
+        searchParamsObject.set("search", searchObject.search);
+      }
     }
 
     allInputElementRefsMap.current.forEach((_, key, map) => {
@@ -90,47 +67,34 @@ export function useSearchPage(): SearchPageHookReturnType {
 
       if (node !== null && node !== undefined && queryParam !== null) {
         node.value = queryParam;
-        searchObjectRef.current = editSearchObjectInLocalStorage(key, queryParam);
-      } else {
-        searchObjectRef.current = editSearchObjectInLocalStorage(key, "");
       }
     });
 
     const maxResultsQueryParam = searchParamsObject.get("maxResults");
-    paginationObjectRef.current.maxResults = handleNumberSearchParam(
-      maxResultsQueryParam,
-      DEFAULT_MAX_RESULTS,
-      DEFAULT_MAX_RESULTS,
+    searchParamsObject.set(
+      "maxResults",
+      handleNumberSearchParam(maxResultsQueryParam, DEFAULT_MAX_RESULTS, DEFAULT_MAX_RESULTS),
     );
-    searchParamsObject.set("maxResults", paginationObjectRef.current.maxResults);
 
     const startIndexQueryParam = searchParamsObject.get("startIndex");
-    paginationObjectRef.current.startIndex = handleNumberSearchParam(
-      startIndexQueryParam,
-      DEFAULT_START_INDEX,
-      DEFAULT_START_INDEX,
+    searchParamsObject.set(
+      "startIndex",
+      handleNumberSearchParam(startIndexQueryParam, DEFAULT_START_INDEX, DEFAULT_START_INDEX),
     );
-    searchParamsObject.set("startIndex", paginationObjectRef.current.startIndex);
+
+    console.log(searchParams.toString());
+    console.log(searchParamsObject.toString());
 
     if (searchParams.toString() !== searchParamsObject.toString()) {
       router.replace(`/search?${searchParamsObject.toString()}`);
     }
-
-    setSearchObjectState(searchObjectRef.current);
-    setPaginationObjectState(paginationObjectRef.current);
   }, [router, searchParams]);
 
   return {
     filters,
     allInputElementRefsMap,
     searchInputElement,
-    searchObjectRef,
-    paginationObjectRef,
     searchParams,
     router,
-    paginationObjectState,
-    setPaginationObjectState,
-    searchObjectState,
-    setSearchObjectState,
   };
 }
