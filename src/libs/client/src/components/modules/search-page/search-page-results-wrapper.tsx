0fc401a11dc94fd67ac0ed2e4d423abc423c6537
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-26 09:16:45 +0000

    chore: render based on search, genre, publisher and pagination values

--- src/libs/client/src/components/modules/search-page/search-page-results-wrapper.tsx
+++ src/libs/client/src/components/modules/search-page/search-page-results-wrapper.tsx
@@ -1,8 +1,20 @@
+import { PaginationObjectType, SearchObjectType } from "@/root/src/libs/shared/src/schemas";
+import { filterKeysArray } from "@/root/src/libs/shared/src/utils";
 import { useSearchParams } from "next/navigation";
 import React from "react";
 
 export default function SearchPageResultsWrapper() {
   const searchParams = useSearchParams();
 
-  return <div>{searchParams.has("run") ? "Can Run babyyyy" : "Cannot run"}</div>;
+  const searchObject: SearchObjectType = {};
+  const paginationObject: PaginationObjectType = {};
+
+  const paramsHasSearch = searchParams.has("search");
+  const paramsHasFilters = filterKeysArray.filter((val) => searchParams.has(val));
+
+  if (!paramsHasSearch && paramsHasFilters.length === 0) {
+    return <div>Please enter a search term or select a filter</div>;
+  }
+
+  return <div>This is the results wrapperrrrr</div>;
 }
