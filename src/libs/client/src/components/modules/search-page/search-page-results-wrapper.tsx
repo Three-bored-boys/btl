06aa02443a54cede03770f66a36341d5ae55199d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-29 23:58:59 +0000

    chore: minor

--- src/libs/client/src/components/modules/search-page/search-page-results-wrapper.tsx
+++ src/libs/client/src/components/modules/search-page/search-page-results-wrapper.tsx
@@ -1,4 +1,4 @@
-import { PaginationObjectType, SearchObjectType } from "@/root/src/libs/shared/src/schemas";
+// import { PaginationObjectType, SearchObjectType } from "@/root/src/libs/shared/src/schemas";
 import { filterKeysArray } from "@/root/src/libs/shared/src/utils";
 import { useSearchParams } from "next/navigation";
 import React from "react";
@@ -6,8 +6,8 @@ import React from "react";
 export default function SearchPageResultsWrapper() {
   const searchParams = useSearchParams();
 
-  const searchObject: SearchObjectType = {};
-  const paginationObject: PaginationObjectType = {};
+  //   const searchObject: SearchObjectType = {};
+  //   const paginationObject: PaginationObjectType = {};
 
   const paramsHasSearch = searchParams.has("search");
   const paramsHasFilters = filterKeysArray.filter((val) => searchParams.has(val));
@@ -15,6 +15,9 @@ export default function SearchPageResultsWrapper() {
   if (!paramsHasSearch && paramsHasFilters.length === 0) {
     return <div>Please enter a search term or select a filter</div>;
   }
+
+  const maxResults = searchParams.get("maxResults");
+  const startIndex = searchParams.get("startIndex");
 
   return <div>This is the results wrapperrrrr</div>;
 }
