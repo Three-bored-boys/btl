ab564352184c4380b88afd02932a74fa289ca2f1
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-30 20:22:46 +0000

    refactor: 'search-page-results-wrapper' component

--- src/libs/client/src/components/modules/search-page/search-page-results-wrapper.tsx
+++ src/libs/client/src/components/modules/search-page/search-page-results-wrapper.tsx
@@ -1,23 +1,43 @@
-// import { PaginationObjectType, SearchObjectType } from "@/root/src/libs/shared/src/schemas";
+import { PaginationObjectType, SearchObjectType } from "@/root/src/libs/shared/src/schemas";
 import { filterKeysArray } from "@/root/src/libs/shared/src/utils";
 import { useSearchParams } from "next/navigation";
 import React from "react";
+import SearchPageResults from "./search-page-results";
 
 export default function SearchPageResultsWrapper() {
   const searchParams = useSearchParams();
 
-  //   const searchObject: SearchObjectType = {};
-  //   const paginationObject: PaginationObjectType = {};
+  const searchObject: SearchObjectType = {};
+  const paginationObject: PaginationObjectType = {};
 
   const paramsHasSearch = searchParams.has("search");
   const paramsHasFilters = filterKeysArray.filter((val) => searchParams.has(val));
 
   if (!paramsHasSearch && paramsHasFilters.length === 0) {
-    return <div>Please enter a search term or select a filter</div>;
+    return <div>Please enter a search term or select a filter and click &quot;Submit&quot;</div>;
   }
 
-  const maxResults = searchParams.get("maxResults");
-  const startIndex = searchParams.get("startIndex");
+  const searchQueryParam = searchParams.get("search");
+  if (searchQueryParam !== null) {
+    searchObject.search = searchQueryParam;
+  }
 
-  return <div>This is the results wrapperrrrr</div>;
+  filterKeysArray.forEach((val) => {
+    const filterParam = searchParams.get(val);
+    if (filterParam !== null) {
+      searchObject[val] = filterParam;
+    }
+  });
+
+  const maxResults = searchParams.get("maxResults");
+  if (maxResults !== null) {
+    paginationObject.maxResults = maxResults;
+  }
+
+  const startIndex = searchParams.get("startIndex");
+  if (startIndex !== null) {
+    paginationObject.startIndex = startIndex;
+  }
+
+  return <SearchPageResults searchObject={searchObject} paginationObject={paginationObject} />;
 }
