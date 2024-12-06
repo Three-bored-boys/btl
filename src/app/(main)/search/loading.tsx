259dbc29f33988d5baea80c84e8fdd23bb07836c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-06 19:35:44 +0000

    style: add Spinner to loading page for '/search' route

--- src/app/(main)/search/loading.tsx
+++ src/app/(main)/search/loading.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { SearchPageResultsLoadingSkeleton } from "@/client/components/modules/search-page/search-page-results-loading-skeleton";
 import { Container } from "@/root/src/libs/client/src/components/layouts/container";
-import { Skeleton } from "@radix-ui/themes";
+import { Spinner } from "@radix-ui/themes";
 
 export default function SearchLoading() {
   return (
     <div>
       <Container>
-        <div className="mb-3 w-full">
-          <Skeleton className="block h-6 w-full" />
+        <div className="mb-3 flex h-52 w-full items-center justify-center">
+          <Spinner size={"3"}></Spinner>
         </div>
         <SearchPageResultsLoadingSkeleton />
       </Container>
