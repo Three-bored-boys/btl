bc853313bb33d58309fb060763a88bee32966ef2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-06 19:22:31 +0000

    style: loading page for '/search' route

--- src/app/(main)/search/loading.tsx
+++ src/app/(main)/search/loading.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { SearchPageResultsLoadingSkeleton } from "@/client/components/modules/search-page/search-page-results-loading-skeleton";
+import { Container } from "@/root/src/libs/client/src/components/layouts/container";
+import { Skeleton } from "@radix-ui/themes";
 
 export default function SearchLoading() {
-  return <div>Yo...</div>;
+  return (
+    <div>
+      <Container>
+        <div className="mb-3 w-full">
+          <Skeleton className="block h-6 w-full" />
+        </div>
+        <SearchPageResultsLoadingSkeleton />
+      </Container>
+    </div>
+  );
 }
