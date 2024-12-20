8d53a4e43959e7781f243ba831ab2f6c6e15fa6d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-20 19:23:06 +0000

    chore: make building book page loading skeleton easier

--- src/app/(main)/books/[isbn]/page.tsx
+++ src/app/(main)/books/[isbn]/page.tsx
@@ -1,13 +1,19 @@
 import { BookPage } from "@/root/src/libs/client/src/components/modules/book-page/book-page";
 import { fetchData } from "@/root/src/libs/client/src/utils";
 import type { Book } from "@/root/src/libs/shared/src/types";
+import { BookPageLoadingSkeleton } from "@/client/components/modules/book-page/book-page-loading-skeleton";
 
 export default async function Book({ params: { isbn } }: { params: { isbn: string } }) {
   const bookObject = await fetchData<Book[]>(`${process.env.API_URL}/books/isbn/${isbn}`);
 
   if (bookObject.length === 0) {
-    return <div>No book Found</div>;
+    return <BookPageLoadingSkeleton />;
   }
 
-  return <BookPage book={bookObject[0]} />;
+  return (
+    <>
+      <BookPage book={bookObject[0]} />
+      <BookPageLoadingSkeleton />
+    </>
+  );
 }
