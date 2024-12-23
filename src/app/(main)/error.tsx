ff72aaab947b49e8360cd703b320cd01dc015e71
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-23 16:18:36 +0000

    chore: update error.tsx for (main) route group

--- src/app/(main)/error.tsx
+++ src/app/(main)/error.tsx
@@ -2,13 +2,17 @@
 
 import { Container } from "@/libs/client/src/components/layouts/container";
 import { Button } from "@/libs/client/src/components/ui/button";
+import { ErrorMessageObject } from "../../libs/client/src/utils";
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  const errorObj = JSON.parse(error.message) as ErrorMessageObject;
+  error.digest = errorObj.message;
+
   return (
     <div>
       <Container>
         <h2>Something went wrong!</h2>
-        <p>{error.message}</p>
+        <p>{errorObj.message}</p>
         <Button background={"dark"} onClick={() => reset()} textSize={"big"}>
           Try again
         </Button>
