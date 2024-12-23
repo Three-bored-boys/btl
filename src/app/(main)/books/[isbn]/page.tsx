de977f8e361a2ea7e78a962f963a92271f8461ef
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-23 16:20:01 +0000

    refactor: book page to not check if the books array is empty

--- src/app/(main)/books/[isbn]/page.tsx
+++ src/app/(main)/books/[isbn]/page.tsx
@@ -5,9 +5,5 @@ import type { Book } from "@/root/src/libs/shared/src/types";
 export default async function Book({ params: { isbn } }: { params: { isbn: string } }) {
   const bookObject = await fetchData<Book[]>(`${process.env.API_URL}/books/isbn/${isbn}`);
 
-  if (bookObject.length === 0) {
-    return <div>No book Found</div>;
-  }
-
   return <BookPage book={bookObject[0]} />;
 }
