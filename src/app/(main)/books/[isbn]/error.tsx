a0f9f01a81c1f96fb46215e92a4c4b3e56376d8d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-23 16:32:51 +0000

    fix: application error that does not render book error page

--- src/app/(main)/books/[isbn]/error.tsx
+++ src/app/(main)/books/[isbn]/error.tsx
@@ -5,6 +5,7 @@ import { LinkButton } from "@/root/src/libs/client/src/components/ui/link-button
 import notFoundImage from "@/public/assets/images/not-found.webp";
 import Image from "next/image";
 import { ErrorMessageObject } from "@/root/src/libs/client/src/utils";
+import { Button } from "@/root/src/libs/client/src/components/ui/button";
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   const errorObj = JSON.parse(error.message) as ErrorMessageObject;
@@ -17,9 +18,12 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
           <p className="mb-3 text-8xl font-extralight md:mb-9 md:text-9xl">{errorObj.statusCode}</p>
           <h2 className="mb-2 text-4xl radix-xs:text-5xl md:mb-5 md:text-7xl">Oops! Something has gone wrong!</h2>
           <p className="mb-8 text-base radix-xs:text-xl md:mb-3 md:text-2xl">{errorObj.message}</p>
-          <LinkButton href="/" background={"light"} textSize={"big"} className="mb-28 radix-xs:mb-9">
+          <LinkButton href="/" background={"light"} textSize={"big"} className="mb-2">
             Return Home
           </LinkButton>
+          <Button background={"dark"} onClick={() => reset()} textSize={"big"}>
+            Try again
+          </Button>
           <div>
             <Image src={notFoundImage} alt="Cartoon image of man sitting on floor and reading a book"></Image>
           </div>
