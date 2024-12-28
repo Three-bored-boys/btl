1fb6d1e5bd6fef2a0e726d6f5c1dede8b8313432
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-28 10:13:01 +0000

    style: border changes to NavBar and Footer

--- src/libs/client/src/components/layouts/footer/footer.tsx
+++ src/libs/client/src/components/layouts/footer/footer.tsx
@@ -4,7 +4,7 @@ import { Container } from "../container";
 
 export function Footer(): React.ReactElement {
   return (
-    <footer className="mt-auto w-full py-5">
+    <footer className="mt-auto w-full border-t border-t-secondary-100 py-5">
       <Container className="flex items-center justify-center gap-3">
         <Logo />
         <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
