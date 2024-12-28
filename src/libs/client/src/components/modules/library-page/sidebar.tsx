fe45d5070c45b86774f2a5c008e3f57f1ece6112
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-28 11:50:47 +0000

    chore: minor edits to sidebar nav

--- src/libs/client/src/components/modules/library-page/sidebar.tsx
+++ src/libs/client/src/components/modules/library-page/sidebar.tsx
@@ -1,33 +1,17 @@
 import React from "react";
 import { usePathname } from "next/navigation";
-import Link from "next/link";
-import { bookLocations, cn } from "@/client/utils";
+import { bookLocations } from "@/client/utils";
+import { SidebarLink } from "@/client/components/ui/sidebar-link";
 
 export function Sidebar(props: React.ComponentProps<"nav">) {
   const pathname = usePathname();
   console.log(pathname);
 
   return (
-    <nav {...props} className="flex w-full flex-col gap-1">
-      <Link
-        href="/library/"
-        className={cn("rounded-lg px-2", {
-          "bg-primary-100": pathname === "/library",
-          "hover:bg-primary-50": pathname !== "/library",
-        })}
-        title="Overview"
-      >
-        Overview
-      </Link>
+    <nav {...props} className="flex h-full w-full flex-col gap-3">
+      <SidebarLink href="/library" currentPathname={pathname} name="Overview" />
       {bookLocations.map((location, i) => (
-        <Link
-          href={`/library/${location.value}`}
-          key={i}
-          title={location.name}
-          className={cn("rounded-lg px-2", { "bg-primary-200": pathname === `/library/${location.value}` })}
-        >
-          {location.name}
-        </Link>
+        <SidebarLink href={`/library/${location.value}`} key={i} name={location.name} currentPathname={pathname} />
       ))}
     </nav>
   );
