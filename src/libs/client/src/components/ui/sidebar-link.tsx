53193ce1a4e7315b9320d108d735d4c7260a19f0
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-12-28 11:50:14 +0000

    feat: create Sidebar link component

---
+++ src/libs/client/src/components/ui/sidebar-link.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link, { LinkProps } from "next/link";
+import { cn } from "../../utils";
+
+type SidebarLinkProps = { currentPathname: string; name: string } & LinkProps & React.ComponentProps<"a">;
+
+export function SidebarLink({ currentPathname, name, href }: SidebarLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={cn("rounded-lg px-4 py-1", {
+        "bg-primary-100": currentPathname === href,
+        "hover:bg-primary-50": currentPathname !== href,
+      })}
+      title={name}
+    >
+      {name}
+    </Link>
+  );
+}
