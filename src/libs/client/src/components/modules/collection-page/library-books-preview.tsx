b117767255b22bd878aa5914dae3c4586448cd95
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-07 23:12:10 +0000

    refactor: edit LibraryBooksPreview when 'showSidebar' is false

--- src/libs/client/src/components/modules/collection-page/library-books-preview.tsx
+++ src/libs/client/src/components/modules/collection-page/library-books-preview.tsx
@@ -13,14 +13,14 @@ export function LibraryBooksPreview({ books, showSidebar }: { books: Book[]; sho
   }
 
   return (
-    // <div className="flex items-center justify-start gap-5">
-    <div className={cn("grid grid-cols-5", { "gap-4 sm:gap-1": showSidebar })}>
+    <div className={cn("grid grid-cols-5 gap-4 sm:gap-1")}>
       {books.map((book, i) => (
         <div key={i}>
           <Link
             href={`/books/${book.isbn13 || book.isbn10}`}
-            className={cn({
-              "block w-full sm:w-28 md:w-24 min-[896px]:w-28 lg:w-32 xl:w-40": showSidebar,
+            className={cn("block w-full sm:w-28", {
+              "md:w-24 min-[896px]:w-28 lg:w-32 xl:w-40": showSidebar,
+              "md:w-28 min-[896px]:w-36 lg:w-40 xl:w-44": !showSidebar,
             })}
           >
             <div className="w-full">
