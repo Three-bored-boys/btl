cfe69c2404b6b4437f923ca46440fba0b0c6470f
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-09 18:01:55 +0000

    chore: add Overview page heading

--- src/libs/client/src/components/modules/collection-page/overview-page.tsx
+++ src/libs/client/src/components/modules/collection-page/overview-page.tsx
@@ -12,6 +12,7 @@ export function OverviewPage() {
 
   return (
     <div>
+      <h1 className="mb-4 font-semibold md:mb-7">Overview</h1>
       {bookLocations.map((location, i) => (
         <OverviewLibraryPreviewSection name={location.name} slug={location.value} books={books[i]} key={i} />
       ))}
