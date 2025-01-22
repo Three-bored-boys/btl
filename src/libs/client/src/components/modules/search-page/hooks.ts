b8aad83bdd22011c0ee45cddd7add3d17ea57be4
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-22 18:19:46 +0000

    chore: return if error is CustomAPIError and throw if not

--- src/libs/client/src/components/modules/search-page/hooks.ts
+++ src/libs/client/src/components/modules/search-page/hooks.ts
@@ -1,5 +1,5 @@
 import { PaginationObjectType, SearchObjectType } from "@/root/src/libs/shared/src/schemas";
-import { fetchData, getSearchObjectFromLocalStorage, handleNumberSearchParam } from "../../../utils";
+import { CustomAPIError, fetchData, getSearchObjectFromLocalStorage, handleNumberSearchParam } from "../../../utils";
 import {
   DEFAULT_MAX_RESULTS,
   DEFAULT_PAGE_NUMBER,
@@ -14,10 +14,17 @@ import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 const getFullSearchResults = async function (searchObject: SearchObjectType, paginationObject: PaginationObjectType) {
-  const results = await fetchData<{ books: Book[]; totalItems: number }>(
-    `${process.env.NEXT_PUBLIC_API_URL}/books/full-search?${new URLSearchParams({ ...searchObject, ...paginationObject }).toString()}`,
-  );
-  return results;
+  try {
+    const results = await fetchData<{ books: Book[]; totalItems: number }>(
+      `${process.env.NEXT_PUBLIC_API_URL}/books/full-search?${new URLSearchParams({ ...searchObject, ...paginationObject }).toString()}`,
+    );
+    return results;
+  } catch (e) {
+    if (e instanceof CustomAPIError) {
+      return e;
+    }
+    throw e;
+  }
 };
 
 export function useSearchPageResults(searchObject: SearchObjectType, paginationObject: PaginationObjectType) {
