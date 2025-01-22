9ed50c0bd1faaffba0283fc74aeb383e0c9c6e4e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-22 18:47:46 +0000

    refactor: better error handling UI for homepage modules

--- src/libs/client/src/components/modules/home-page/error-boundary-render.tsx
+++ src/libs/client/src/components/modules/home-page/error-boundary-render.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "../../ui/button";
+import { ExclamationTriangle } from "../../ui/icons/exclamation-triangle";
 
 export function ErrorBoundaryRender({ error, resetErrorBoundary }: { error: Error; resetErrorBoundary: () => void }) {
   return (
-    <div className="w-full">
-      <p>Something went wrong: {error.message}</p>
+    <div className="my-2 flex w-full flex-col items-center justify-start gap-y-1">
+      <ExclamationTriangle />
+      <p className="text-xl font-semibold">Something went wrong</p>
+      <p className="text-base font-normal">{error.message}</p>
       <Button background={"light"} onClick={resetErrorBoundary} textSize={"big"}>
         Try again
       </Button>
