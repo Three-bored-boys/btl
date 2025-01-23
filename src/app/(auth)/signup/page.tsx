5710169f27df12186bc5583e542d6e9378130a79
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-23 22:48:33 +0000

    style: update '/signup' page.tsx

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,12 +2,13 @@ import { ReactElement } from "react";
 import { SignupForm } from "@/client/components/modules/auth/signup/signup-form";
 import Link from "next/link";
 import React from "react";
+import { AuthPage } from "@/client/components/modules/auth/auth-page";
 
-export default function Signup(): ReactElement {
+export default function SignupPage(): ReactElement {
   return (
-    <>
+    <AuthPage>
       <SignupForm />
       <Link href={"/login"}>Go to Log In page</Link>
-    </>
+    </AuthPage>
   );
 }
