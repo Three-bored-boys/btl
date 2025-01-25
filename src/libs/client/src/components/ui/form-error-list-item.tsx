f5a49c855c6fd3ebcbe8de69b82d775b7ca56a64
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-25 17:07:22 +0000

    chore: component rename to 'FormErrorListItem'

--- src/libs/client/src/components/ui/form-error-list-item.tsx
+++ src/libs/client/src/components/ui/form-error-list-item.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const FormFieldErrorListItem = function ({
+export const FormErrorListItem = function ({
   children,
   ...props
 }: { children: React.ReactNode } & React.ComponentProps<"li">) {
