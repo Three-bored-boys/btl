976a11177e4308b2507e83c83123d94cf8886163
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-25 01:04:04 +0000

    feat: update signup-form.tsx

--- src/libs/client/src/components/modules/auth/signup/signup-form.tsx
+++ src/libs/client/src/components/modules/auth/signup/signup-form.tsx
@@ -3,37 +3,116 @@
 import React from "react";
 import { Label } from "@/client/components/ui/label";
 import { Input } from "@/client/components/ui/input";
-import { Button } from "../../../ui/button";
+import { FormFieldErrorListItem } from "@/client/components/ui/form-field-error-list-item";
+import { Button } from "@/client/components/ui/button";
+import { signupSchema, SignupFormState } from "@/libs/shared/src/schemas";
+import { useRouter } from "next/navigation";
 
 export function SignupForm() {
-  const onFormSubmission = function (e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log("Signed Up!");
+  const router = useRouter();
+  const [signupFormState, setSignupFormState] = React.useState<SignupFormState>({
+    fieldError: { userName: [], emailAddress: [], password: [] },
+    formStatus: null,
+  });
+
+  const updateSignupFormState = function (event: React.FormEvent<HTMLFormElement>): SignupFormState {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const signupObj = Object.fromEntries(formData);
+
+    const validation = signupSchema.safeParse(signupObj);
+
+    if (!validation.success) {
+      return {
+        fieldError: {
+          userName: validation.error.issues
+            .filter((issue) => issue.path[0] === "userName")
+            .map((issue) => issue.message),
+          emailAddress: validation.error.issues
+            .filter((issue) => issue.path[0] === "emailAddress")
+            .map((issue) => issue.message),
+          password: validation.error.issues
+            .filter((issue) => issue.path[0] === "password")
+            .map((issue) => issue.message),
+        },
+        formStatus: null,
+      };
+    }
+
+    return {
+      fieldError: { userName: [], emailAddress: [], password: [] },
+      formStatus: { success: true, message: "Success!" },
+    };
+  };
+
+  const onSubmit = function (e: React.FormEvent<HTMLFormElement>) {
+    const newFormState = updateSignupFormState(e);
+    setSignupFormState(newFormState);
+
+    if (!newFormState.formStatus?.success) {
+      return;
+    }
+
+    router.push("/");
+  };
+
+  const FormStatusMessage = function ({
+    formStatus,
+  }: {
+    formStatus: { success: false; error: string } | { success: true; message: string };
+  }) {
+    if (formStatus.success) {
+      return <p>{formStatus.message}</p>;
+    } else {
+      return <p>{formStatus.error}</p>;
+    }
   };
 
   return (
-    <form className="flex w-full flex-col" id="logInForm" onSubmit={onFormSubmission}>
-      <Label htmlFor="firstName">First Name:</Label>
-      <Input id="firstName" type="text" className="mb-3" />
+    <form
+      className="flex w-full flex-col"
+      id="logInForm"
+      onSubmit={onSubmit}
+      onChange={(e) => setSignupFormState(updateSignupFormState(e))}
+    >
+      <div className="mb-6 flex flex-col">
+        <Label htmlFor="userName">Username:</Label>
+        <Input id="userName" type="text" name="userName" />
+        <ul style={{ listStyle: "disc", listStylePosition: "outside" }}>
+          {signupFormState.fieldError.userName.map((error, i) => (
+            <FormFieldErrorListItem key={i}>{error}</FormFieldErrorListItem>
+          ))}
+        </ul>
+      </div>
 
-      <Label htmlFor="middleName">Middle Name&#40;s&#41;:</Label>
-      <Input id="middleName" type="text" className="mb-3" />
+      <div className="mb-6 flex flex-col">
+        <Label htmlFor="emailAddress">Email address:</Label>
+        <Input id="emailAddress" type="email" name="emailAddress" />
+        <ul style={{ listStyle: "disc", listStylePosition: "outside" }}>
+          {signupFormState.fieldError.emailAddress.map((error, i) => (
+            <FormFieldErrorListItem key={i}>{error}</FormFieldErrorListItem>
+          ))}
+        </ul>
+      </div>
 
-      <Label htmlFor="lastName">Last Name:</Label>
-      <Input id="lastName" type="text" className="mb-3" />
+      <div className="mb-6 flex flex-col">
+        <Label htmlFor="password">Password:</Label>
+        <Input id="password" type="password" name="password" />
+        <ul style={{ listStyle: "disc", listStylePosition: "outside" }}>
+          {signupFormState.fieldError.password.map((error, i) => (
+            <FormFieldErrorListItem key={i}>{error}</FormFieldErrorListItem>
+          ))}
+        </ul>
+      </div>
 
-      <Label htmlFor="userName">User Name:</Label>
-      <Input id="userName" type="text" className="mb-3" />
-
-      <Label htmlFor="emailAddress">Email address:</Label>
-      <Input id="emailAddress" type="email" className="mb-3" />
-
-      <Label htmlFor="password">Password:</Label>
-      <Input id="password" type="password" className="mb-3" />
-
-      <Button background={"light"} type="submit" disabled={false} textSize={"big"}>
-        Sign Up
-      </Button>
+      <div>
+        <Button background={"light"} type="submit" disabled={false} textSize={"big"}>
+          Sign Up
+        </Button>
+      </div>
+      {signupFormState.formStatus !== null && (
+        <FormStatusMessage formStatus={signupFormState.formStatus}></FormStatusMessage>
+      )}
     </form>
   );
 }
