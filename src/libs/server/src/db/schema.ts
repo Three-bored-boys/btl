c503a4399931e6aef60e67912ef9f09de015f782
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-26 18:04:24 +0000

    chore: add 'libraries' database table

--- src/libs/server/src/db/schema.ts
+++ src/libs/server/src/db/schema.ts
@@ -1,4 +1,5 @@
-import { boolean, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { boolean, pgTable, serial, text, timestamp, pgEnum } from "drizzle-orm/pg-core";
+import { bookLibraries } from "@/libs/shared/src/utils";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -16,4 +17,16 @@ export const sessions = pgTable("sessions", {
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
   expiresAt: timestamp("expires_at", { mode: "date", withTimezone: true }).notNull(),
+});
+
+const libraryNames = bookLibraries.map((lib) => lib.name) as [string, ...string[]];
+const libraryValues = bookLibraries.map((lib) => lib.value) as [string, ...string[]];
+
+export const libraryNameEnum = pgEnum("library_name", libraryNames);
+export const libraryValueEnum = pgEnum("library_value", libraryValues);
+
+export const libraries = pgTable("libraries", {
+  id: serial("id").primaryKey(),
+  libraryName: libraryNameEnum("library_name").notNull(),
+  libraryValue: libraryValueEnum("library_value").notNull(),
 });
