b044f8a90cdbc006268b53448d740ce30699ad37
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-26 17:01:00 +0000

    chore: update users table definition and add sessions table definition

--- src/libs/server/src/db/schema.ts
+++ src/libs/server/src/db/schema.ts
@@ -1,6 +1,19 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { boolean, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
-  fullName: text("full_name"),
+  userName: text("user_name").notNull().unique(),
+  emailAddress: text("email_address").notNull().unique(),
+  emailVerified: boolean("email_verified").notNull().default(false),
+  hashedPassword: text("hashed_password").notNull(),
+  createdAt: timestamp("created_at", { mode: "date", withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "date", withTimezone: true }).notNull().defaultNow(),
+});
+
+export const sessions = pgTable("sessions", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expiresAt: timestamp("expires_at", { mode: "date", withTimezone: true }).notNull(),
 });
