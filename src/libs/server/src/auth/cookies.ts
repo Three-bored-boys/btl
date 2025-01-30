4c1369451b8903ee848d2cc8e4482fb6d6578ef8
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-01-30 23:03:18 +0000

    chore: create cookies.ts

---
+++ src/libs/server/src/auth/cookies.ts
@@ -0,0 +1,23 @@
+import { getCookie, setCookie, deleteCookie } from "hono/cookie";
+import { Context } from "hono";
+import { BTL_AUTH_SESSION } from "@/server/utils";
+
+export const setSessionCookie = function (c: Context, token: string, expiresAt: Date) {
+  setCookie(c, BTL_AUTH_SESSION, token, {
+    expires: expiresAt,
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+    path: "/",
+  });
+};
+
+export const getSessionCookieToken = function (c: Context) {
+  const token = getCookie(c, BTL_AUTH_SESSION);
+  if (!token) return null;
+  return token;
+};
+
+export const deleteSessionCookie = function (c: Context) {
+  deleteCookie(c, BTL_AUTH_SESSION);
+};
