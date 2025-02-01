f79558e712dc0bb0e54e64f4acd2035e2a71b557
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-01 22:10:10 +0000

    refactor: create fetchServiceData wrapper function

--- src/libs/server/src/utils.ts
+++ src/libs/server/src/utils.ts
@@ -6,4 +6,16 @@ export const sanitizedUser = function (user: User): SanitizedUser {
   return rest;
 };
 
+export const fetchServiceData = async function <T>(url: string, options?: RequestInit) {
+  const res = await fetch(url, options);
+  console.log(res.ok, res.status, res.statusText);
+
+  if (!res.ok) {
+    throw new Error();
+  }
+
+  const data = (await res.json()) as T;
+  return data;
+};
+
 export const BTL_AUTH_SESSION = "btl_auth_session";
