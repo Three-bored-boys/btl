43d54679498e6792324cc38a15071c4d3b7b72c9
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-03 17:22:35 +0000

    build: move drizzle migrationClient due to workers environment

--- src/libs/server/src/db/index.ts
+++ src/libs/server/src/db/index.ts
@@ -1,11 +1,5 @@
-import { config } from "dotenv";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
-config({ path: ".dev.vars" });
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const db = drizzle(postgres(process.env.DATABASE_URL!));
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-export const migrationClient = drizzle(postgres(process.env.DATABASE_URL!, { max: 1 }));
