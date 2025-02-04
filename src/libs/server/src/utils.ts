f7ca2c7ea4eceb9ef1015d8e879a7a0b97ee9e9f
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-04 09:57:07 +0000

    chore: move all auth util functions to auth/utils.ts

--- src/libs/server/src/utils.ts
+++ src/libs/server/src/utils.ts
@@ -18,33 +18,4 @@ export const fetchServiceData = async function <T>(url: string, options?: Reques
   return data;
 };
 
-export const generateAuthSessionToken = function () {
-  return crypto.randomUUID();
-};
-
-const uint8ArrayToBase64 = function (array: Uint8Array): string {
-  let binary = "";
-  array.forEach((byte) => (binary += String.fromCharCode(byte)));
-  return btoa(binary);
-};
-
-/* const base64ToUint8Array = function (base64: string): Uint8Array {
-  const binaryString = atob(base64);
-  const bytes = new Uint8Array(binaryString.length);
-  for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes;
-}; */
-
-export const encryptAuthSessionToken = async function (token: string) {
-  const key = await crypto.subtle.generateKey({ name: "AES-GCM", length: 256 }, true, ["encrypt", "decrypt"]);
-  const iv = crypto.getRandomValues(new Uint8Array(12));
-  const encodedToken = new TextEncoder().encode(token);
-  const encryptedToken = await crypto.subtle.encrypt({ name: "AES-GCM", iv }, key, encodedToken);
-  const ivBase64 = uint8ArrayToBase64(iv);
-  const encryptedTokenBase64 = uint8ArrayToBase64(new Uint8Array(encryptedToken));
-  return `${ivBase64}:${encryptedTokenBase64}`;
-};
-
 export const BTL_AUTH_SESSION = "btl_auth_session";
