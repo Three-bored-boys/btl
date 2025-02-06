3c01a9ce4d666978329f75641483a3e07c05db13
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-06 15:58:58 +0000

    refactor: db initialisation. development db is now a neon database

--- src/libs/server/src/db/db.ts
+++ src/libs/server/src/db/db.ts
@@ -1,42 +1,18 @@
-import { drizzle } from "drizzle-orm/neon-serverless";
-import { Client as neonClient, neonConfig } from "@neondatabase/serverless";
-import ws from "ws";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import { type Context } from "hono";
 import { type Environment } from "@/root/bindings";
 
 const createClient = function (c: Context<Environment>) {
-  let connectionString = c.env.DATABASE_URL;
+  const connectionString = c.env.DATABASE_URL;
 
-  if (c.env.ENVIRONMENT === "development") {
-    connectionString = c.env.DATABASE_URL_DEV;
-    neonConfig.fetchEndpoint = (host) => {
-      const [protocol, port] = host === "db.localtest.me" ? ["http", 4444] : ["https", 443];
-      return `${protocol}://${host}:${port.toString()}/sql`;
-    };
-    const connectionStringUrl = new URL(connectionString);
-    neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== "db.localtest.me";
-    neonConfig.wsProxy = (host) => (host === "db.localtest.me" ? `${host}:4444/v2` : `${host}/v2`);
-  }
-  neonConfig.webSocketConstructor = ws;
-
-  const client = new neonClient({ connectionString });
+  const client = neon(connectionString, { fetchOptions: { timeout: 30000 } });
   return client;
 };
 
-export const useDB = async function <T>(
-  c: Context<Environment>,
-  callback: (db: ReturnType<typeof drizzle>) => Promise<T>,
-) {
+export const db = function (c: Context<Environment>) {
   const client = createClient(c);
-  await client.connect();
-
   const dbInstance = drizzle(client);
-  try {
-    const result = await callback(dbInstance);
-    return result;
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.end();
-  }
+
+  return dbInstance;
 };
