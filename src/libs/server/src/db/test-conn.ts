be168cb13ca1bb402a6009a27163023cf6c6d754
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-06 15:52:33 +0000

    chore: create ts file to test db connection

---
+++ src/libs/server/src/db/test-conn.ts
@@ -0,0 +1,22 @@
+import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".dev.vars" });
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+async function testConnection() {
+  try {
+    await client.connect();
+    const res = await client.query("SELECT NOW();");
+    console.log("Connected successfully:", res.rows[0]);
+  } catch (err) {
+    console.error("Connection error:", err);
+  } finally {
+    await client.end();
+  }
+}
+
+void testConnection();
