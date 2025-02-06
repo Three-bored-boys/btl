a12431d2bc5ce46d8c4431716e07f99fd23a9d75
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-06 00:22:26 +0000

    chore: add workers secrets for using neon serverless for DB in local dev

--- bindings.d.ts
+++ bindings.d.ts
@@ -1,7 +1,10 @@
+import { Environment } from "@/root/bindings";
 export type Environment = {
   Bindings: {
     DATABASE_URL: string;
     GOOGLE_BOOKS_API_KEY: string;
     NY_TIMES_BOOKS_API_KEY: string;
+    ENVIRONMENT: string;
+    DATABASE_URL_DEV: string;
   };
 };
