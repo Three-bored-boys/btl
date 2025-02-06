dcb7cc67bbaf7d69690133f5f6ae6ae6665c5a06
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-06 00:41:50 +0000

    refactor: use neon serverless driver in prod and dev, replacing pg

--- src/libs/server/src/db/db.ts
+++ src/libs/server/src/db/db.ts
@@ -1,11 +1,25 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { Client as neonClient, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 import { type Context } from "hono";
-import { Environment } from "@/root/bindings";
+import { type Environment } from "@/root/bindings";
 
 const createClient = function (c: Context<Environment>) {
-  const connectionString = c.env.DATABASE_URL;
-  const client = new Client({ connectionString });
+  let connectionString = c.env.DATABASE_URL;
+
+  if (c.env.ENVIRONMENT === "development") {
+    connectionString = c.env.DATABASE_URL_DEV;
+    neonConfig.fetchEndpoint = (host) => {
+      const [protocol, port] = host === "db.localtest.me" ? ["http", 4444] : ["https", 443];
+      return `${protocol}://${host}:${port.toString()}/sql`;
+    };
+    const connectionStringUrl = new URL(connectionString);
+    neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== "db.localtest.me";
+    neonConfig.wsProxy = (host) => (host === "db.localtest.me" ? `${host}:4444/v2` : `${host}/v2`);
+  }
+  neonConfig.webSocketConstructor = ws;
+
+  const client = new neonClient({ connectionString });
   return client;
 };
 
