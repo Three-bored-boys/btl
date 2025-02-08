229833da24e74fc980b8b44be028f259c939c00c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-08 19:31:25 +0000

    chore: add Environment generic to Context type in cookie helpers

--- src/libs/server/src/auth/cookies.ts
+++ src/libs/server/src/auth/cookies.ts
@@ -1,8 +1,9 @@
 import { getCookie, setCookie, deleteCookie } from "hono/cookie";
 import { Context } from "hono";
 import { BTL_AUTH_SESSION } from "@/server/utils";
+import { Environment } from "@/root/bindings";
 
-export const setSessionCookie = function (c: Context, token: string, expiresAt: Date) {
+export const setSessionCookie = function (c: Context<Environment>, token: string, expiresAt: Date) {
   setCookie(c, BTL_AUTH_SESSION, token, {
     expires: expiresAt,
     secure: true,
@@ -12,12 +13,12 @@ export const setSessionCookie = function (c: Context, token: string, expiresAt: 
   });
 };
 
-export const getSessionCookieToken = function (c: Context) {
+export const getSessionCookieToken = function (c: Context<Environment>) {
   const token = getCookie(c, BTL_AUTH_SESSION);
   if (!token) return null;
   return token;
 };
 
-export const deleteSessionCookie = function (c: Context) {
+export const deleteSessionCookie = function (c: Context<Environment>) {
   deleteCookie(c, BTL_AUTH_SESSION);
 };
