46b5cdac5c42355bbd9f4fd69d3f5fe7bae08e06
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-10 19:04:54 +0000

    chore: change type to include status with BadResponse

--- src/libs/shared/src/types/response.ts
+++ src/libs/shared/src/types/response.ts
@@ -6,4 +6,6 @@ export type GoodResponse<T = NonNullable<unknown>> = {
 export type BadResponse = { success: false; errors: string[] };
 
 export type HandlerResult<P> = { handlerResult: BadResponse | GoodResponse<P> }; // For the fetch route handler custom functon in app api utils
-export type FetchDataResult<P> = { fetchDataResult: BadResponse | GoodResponse<P> }; // For the fetch data custom function in client utils
+
+export type FetchDataResult<P> = { fetchDataResult: BadResponseWithStatus | GoodResponse<P> }; // For the fetch data custom function in client utils
+export type BadResponseWithStatus = BadResponse & { status: number };
