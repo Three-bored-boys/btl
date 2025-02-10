befa2333ca2175454dc5f275426b00f0746edd55
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-10 18:09:48 +0000

    chore: add new return type for the fetchData custom function

--- src/libs/shared/src/types/response.ts
+++ src/libs/shared/src/types/response.ts
@@ -5,4 +5,5 @@ export type GoodResponse<T = NonNullable<unknown>> = {
 
 export type BadResponse = { success: false; errors: string[] };
 
-export type HandlerResult<P> = { handlerResult: BadResponse | GoodResponse<P> }; // For the route handler specifically
+export type HandlerResult<P> = { handlerResult: BadResponse | GoodResponse<P> }; // For the fetch route handler custom functon in app api utils
+export type FetchDataResult<P> = { fetchDataResult: BadResponse | GoodResponse<P> }; // For the fetch data custom function in client utils
