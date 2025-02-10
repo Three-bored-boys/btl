cd86eed673f6db552fa99a589315070674ff425e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-10 22:38:26 +0000

    refactor: the getUser function

--- src/libs/client/src/components/modules/auth/utils.ts
+++ src/libs/client/src/components/modules/auth/utils.ts
@@ -1,15 +1,9 @@
-import { CustomAPIError, fetchData } from "@/client/utils";
+import { fetchData } from "@/client/utils";
 import { SanitizedUser } from "@/root/src/libs/shared/src/db/schema";
 
 export const getUser = async function () {
   const apiURL = process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL;
-  try {
-    const user = await fetchData<SanitizedUser>(`${apiURL}/auth/validate-session`);
-    return user;
-  } catch (e) {
-    if (e instanceof CustomAPIError) {
-      return null;
-    }
-    throw e;
-  }
+
+  const { fetchDataResult } = await fetchData<SanitizedUser>(`${apiURL}/auth/validate-session`);
+  return fetchDataResult;
 };
