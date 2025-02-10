9e5448c3b0a38306c79591b6d2459fd710fea5a2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-10 07:45:20 +0000

    refactor: signup route handler to stop using fetchData

--- src/app/api/(auth)/signup/route.ts
+++ src/app/api/(auth)/signup/route.ts
@@ -1,37 +1,58 @@
-import { CustomAPIError, fetchData } from "@/root/src/libs/client/src/utils";
-import { SanitizedUser } from "@/root/src/libs/shared/src/db/schema";
 import type { SignupInput } from "@/root/src/libs/shared/src/validators";
 import { type NextRequest, NextResponse } from "next/server";
-import { HandlerResult } from "@/shared/types";
+import { BadResponse, GoodResponse, HandlerResult } from "@/shared/types";
 import { SignupResult } from "@/shared/validators/auth";
 
 export async function POST(req: NextRequest): Promise<NextResponse<HandlerResult<SignupResult>>> {
+  let response = new NextResponse<HandlerResult<SignupResult>>();
+  let setCookie: string | null = null;
+  const signupInput = (await req.json()) as SignupInput;
   try {
-    const signupInput = (await req.json()) as SignupInput;
-    const data = await fetchData<{ message: string; user: SanitizedUser }>(`${process.env.API_URL}/auth/signup`, {
+    const externalResponse = await fetch(`${process.env.API_URL}/auth/signup`, {
       method: "POST",
       body: JSON.stringify(signupInput),
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
     });
 
-    return NextResponse.json({ handlerResult: { success: true, data } });
-  } catch (e) {
-    if (e instanceof CustomAPIError) {
-      return NextResponse.json(
-        { handlerResult: { success: false, errors: e.errors } },
+    setCookie = externalResponse.headers.get("set-cookie");
+
+    if (!externalResponse.ok) {
+      const errorObj = (await externalResponse.json()) as BadResponse;
+
+      response = NextResponse.json(
+        { handlerResult: errorObj },
         {
-          status: e.status,
+          status: externalResponse.status,
         },
       );
+
+      if (setCookie) {
+        response.headers.set("set-cookie", setCookie);
+      }
+
+      return response;
     }
 
-    return NextResponse.json(
-      { handlerResult: { success: false, errors: ["Something went wrong. Please try again later."] } },
+    const data = (await externalResponse.json()) as GoodResponse<SignupResult>;
+
+    response = NextResponse.json({ handlerResult: data });
+
+    if (setCookie) {
+      response.headers.set("set-cookie", setCookie);
+    }
+
+    return response;
+  } catch (e) {
+    response = NextResponse.json(
+      { handlerResult: { success: false, errors: ["Something went wrong. Please try again."] } },
       {
         status: 500,
       },
     );
+
+    return response;
   }
 }
