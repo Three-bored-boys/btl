263e5f0c639fc856da8689424ed05b4f8f81e7d2
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-12 19:02:02 +0000

    chore: rename btl session cookie name and move it to shared lib

--- src/libs/server/src/auth/cookies.ts
+++ src/libs/server/src/auth/cookies.ts
@@ -1,10 +1,10 @@
 import { getCookie, setCookie, deleteCookie } from "hono/cookie";
 import { Context } from "hono";
-import { BTL_AUTH_SESSION } from "@/server/utils";
+import { BTL_AUTH_SESSION_COOKIE_NAME } from "@/shared/utils";
 import { Environment } from "@/root/bindings";
 
 export const setSessionCookie = function (c: Context<Environment>, token: string, expiresAt: Date) {
-  setCookie(c, BTL_AUTH_SESSION, token, {
+  setCookie(c, BTL_AUTH_SESSION_COOKIE_NAME, token, {
     expires: expiresAt,
     secure: true,
     httpOnly: true,
@@ -14,11 +14,11 @@ export const setSessionCookie = function (c: Context<Environment>, token: string
 };
 
 export const getSessionCookieToken = function (c: Context<Environment>) {
-  const token = getCookie(c, BTL_AUTH_SESSION);
+  const token = getCookie(c, BTL_AUTH_SESSION_COOKIE_NAME);
   if (!token) return null;
   return token;
 };
 
 export const deleteSessionCookie = function (c: Context<Environment>) {
-  deleteCookie(c, BTL_AUTH_SESSION);
+  deleteCookie(c, BTL_AUTH_SESSION_COOKIE_NAME);
 };
