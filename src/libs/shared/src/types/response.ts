696e5d0027e5ee80066256dcb559bae4eb0d5d49
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-13 22:07:41 +0000

    chore: rename type HandlerResult to ServerResult

--- src/libs/shared/src/types/response.ts
+++ src/libs/shared/src/types/response.ts
@@ -5,6 +5,6 @@ export type GoodResponse<T = NonNullable<unknown>> = {
 
 export type BadResponse = { success: false; errors: string[] };
 
-export type HandlerResult<P> = { handlerResult: BadResponse | GoodResponse<P> }; // For the fetch route handler custom functon in app api utils
+export type ServerResult<P> = { serverResult: BadResponse | GoodResponse<P> }; // For the fetch route handler custom functon in app api utils
 
 export type FetchDataResult<T> = { fetchDataResult: BadResponse | GoodResponse<T>; res: Response }; // For the fetch data custom function in client utils
