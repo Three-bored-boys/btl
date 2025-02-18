04267c19492ee1e037e59a72dcdd52a0b7898957
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-18 15:03:48 +0000

    chore: create base UI for when user is logged in

--- src/libs/client/src/components/layouts/navigation-bar/nav-bar-right.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/nav-bar-right.tsx
@@ -1,26 +1,33 @@
 import React, { ComponentProps } from "react";
 import type { NavAuthLinkArr } from "./nav-bar";
 import { LinkButton } from "@/client/components/ui/link-button";
-import { cn } from "../../../utils";
+import { cn } from "@/client/utils";
+import { useAuthContext } from "@/client/providers/auth-context-provider";
 
 type NavBarRightProps = {
   routesArr: NavAuthLinkArr;
 } & ComponentProps<"div">;
 
 export function NavBarRight({ routesArr, className }: NavBarRightProps): React.ReactElement {
-  return (
-    <div className={cn("m-auto", className)}>
-      <LinkButton background={"dark"} href={routesArr[0].path} textSize={"big"}>
-        {routesArr[0].name}
-      </LinkButton>
-      <LinkButton
-        background={"light"}
-        href={routesArr[1].path}
-        className="text ml-2 hidden md:inline lg:ml-4"
-        textSize={"big"}
-      >
-        {routesArr[1].name}
-      </LinkButton>
-    </div>
-  );
+  const { user } = useAuthContext();
+
+  if (!user) {
+    return (
+      <div className={cn("m-auto", className)}>
+        <LinkButton background={"dark"} href={routesArr[0].path} textSize={"big"}>
+          {routesArr[0].name}
+        </LinkButton>
+        <LinkButton
+          background={"light"}
+          href={routesArr[1].path}
+          className="text ml-2 hidden md:inline lg:ml-4"
+          textSize={"big"}
+        >
+          {routesArr[1].name}
+        </LinkButton>
+      </div>
+    );
+  }
+
+  return <div className="ml-20">Hi, {user.userName}</div>;
 }
