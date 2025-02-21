ddf9e982d8316223485becbaefbbcd86118bdc53
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-21 22:22:50 +0000

    style: create basic structure for dropdown menu

--- src/libs/client/src/components/layouts/navigation-bar/nav-bar-right.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/nav-bar-right.tsx
@@ -3,7 +3,7 @@ import type { NavAuthLinkArr } from "./nav-bar";
 import { LinkButton } from "@/client/components/ui/link-button";
 import { cn } from "@/client/utils";
 import { useAuthContext } from "@/client/providers/auth-context-provider";
-import { Avatar } from "@radix-ui/themes";
+import { Avatar, DropdownMenu } from "@radix-ui/themes";
 
 type NavBarRightProps = {
   routesArr: NavAuthLinkArr;
@@ -30,5 +30,19 @@ export function NavBarRight({ routesArr, className }: NavBarRightProps): React.R
     );
   }
 
-  return <Avatar fallback>Hi, {user.userName}</Avatar>;
+  return (
+    <div className="ml-1 cursor-pointer md:ml-10 xl:ml-20">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <div className="flex items-center">
+            <span className="mx-auto my-auto inline-block w-24 truncate xs:w-28">Hi, {user.userName}</span>
+            <Avatar fallback src="" radius="full" className="ml-2"></Avatar>
+          </div>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Item>Hello</DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </div>
+  );
 }
