73b4a33385f21eaea46abc939fe5c68f3970d9f7
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-23 23:23:21 +0000

    chore: rename to 'books-route'

--- src/app/(main)/books/[isbn]/error.tsx
+++ src/app/(main)/books/[isbn]/error.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BookPageErrorBoundaryRender } from "@/root/src/libs/client/src/components/modules/book-page/book-page-error-boundary-render";
+import { BookPageErrorBoundaryRender } from "@/root/src/libs/client/src/components/modules/books-route/book-page-error-boundary-render";
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   return <BookPageErrorBoundaryRender error={error} reset={reset} />;
--- src/app/(main)/books/[isbn]/page.tsx
+++ src/app/(main)/books/[isbn]/page.tsx
@@ -1,4 +1,4 @@
-import { BookPageWrapper } from "@/root/src/libs/client/src/components/modules/book-page/book-page-wrapper";
+import { BookPageWrapper } from "@/root/src/libs/client/src/components/modules/books-route/book-page-wrapper";
 
 export default function Book({ params: { isbn } }: { params: { isbn: string } }) {
   return <BookPageWrapper params={{ isbn }} />;
