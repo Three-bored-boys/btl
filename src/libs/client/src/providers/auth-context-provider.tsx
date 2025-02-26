762292c04bd3bb214b5f88dbe2a5cf30ff1bffe3
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-26 21:58:11 +0000

    refactor: edit auth-context-provider.tsx

--- src/libs/client/src/providers/auth-context-provider.tsx
+++ src/libs/client/src/providers/auth-context-provider.tsx
@@ -3,43 +3,19 @@
 import { createContext } from "react";
 import { SanitizedUser } from "@/shared/db/schema";
 import React from "react";
-import { fetchData } from "@/client/utils";
-import { useQuery } from "@tanstack/react-query";
 import { Spinner } from "@radix-ui/themes";
 import { Logo } from "@/client/components/ui/logo";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../hooks";
 
-const AuthContext = createContext<{
+export const AuthContext = createContext<{
   user: SanitizedUser | null;
   setUser: React.Dispatch<React.SetStateAction<SanitizedUser | null>>;
 } | null>(null);
 
-export const validateUser = async function () {
-  try {
-    const { fetchDataResult } = await fetchData<SanitizedUser>(
-      `${process.env.NEXT_PUBLIC_API_URL}/auth/validate-session`,
-      {
-        credentials: "include",
-      },
-    );
-
-    if (!fetchDataResult.success) {
-      return null;
-    }
-
-    const { data } = fetchDataResult;
-    return data;
-  } catch (e) {
-    return null;
-  }
-};
-
-export const useValidateUser = function () {
-  return useQuery({ queryKey: ["btl_session_user"], queryFn: () => validateUser(), staleTime: 0 });
-};
-
 export const AuthContextProvider = function ({ children }: { children: React.ReactNode }) {
   const [user, setUser] = React.useState<SanitizedUser | null>(null);
-  const { data, isLoading } = useValidateUser();
+  const { data, isLoading } = useQuery({ queryKey: ["btl_session_user"], queryFn: () => getUser(), staleTime: 0 });
   const value = React.useMemo(() => ({ user, setUser }), [user]);
   React.useEffect(() => {
     if (data !== undefined) {
@@ -52,16 +28,6 @@ export const AuthContextProvider = function ({ children }: { children: React.Rea
   }
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
-
-export const useAuthContext = function () {
-  const context = React.useContext(AuthContext);
-
-  if (!context) {
-    throw new Error("useAuthContext must be used within a AuthContextProvider");
-  }
-
-  return context;
 };
 
 export const CheckingUserSession = function () {
