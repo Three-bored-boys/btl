ef9cb00bf9ed08f2169d46dba6b6f6e6bfce163a
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-02-26 21:47:32 +0000

    chore: change Overview page back to original layout

--- src/libs/client/src/components/modules/collection-route/overview-page.tsx
+++ src/libs/client/src/components/modules/collection-route/overview-page.tsx
@@ -5,7 +5,6 @@ import { testBook } from "./data";
 import { OverviewLibraryPreviewSection } from "./overview-library-preview-section";
 import { bookLibraries } from "@/shared/utils";
 import { Book } from "@/root/src/libs/shared/src/types";
-import { useAuthContext } from "@/client/providers/auth-context-provider";
 
 export function OverviewPage() {
   const books1 = [testBook, testBook, testBook, testBook, testBook, testBook, testBook, testBook, testBook, testBook];
@@ -13,18 +12,13 @@ export function OverviewPage() {
   const books3 = [testBook, testBook, testBook, testBook, testBook];
   const books4 = [testBook, testBook, testBook, testBook];
   const books = [books1, books2, books3, books4];
-  const { user } = useAuthContext();
 
   return (
     <div>
       <h1 className="font-semibold">Overview</h1>
-      {!user ? (
-        <div className="flex w-full items-center justify-center">Login now!</div>
-      ) : (
-        bookLibraries.map((library, i) => (
-          <OverviewLibraryPreviewSection name={library.name} slug={library.value} books={books[i]} key={i} />
-        ))
-      )}
+      {bookLibraries.map((library, i) => (
+        <OverviewLibraryPreviewSection name={library.name} slug={library.value} books={books[i]} key={i} />
+      ))}
     </div>
   );
 }
