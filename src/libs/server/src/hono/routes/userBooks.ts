c516a3f847679568aa0b3f4d243822de58159c03
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-05 14:51:56 +0000

    feat: add route to add or update entry in user_books db table

--- src/libs/server/src/hono/routes/userBooks.ts
+++ src/libs/server/src/hono/routes/userBooks.ts
@@ -4,7 +4,10 @@ import { db } from "@/server/db/db";
 import { SanitizedUser, userBooks, UserBook } from "@/server/db/schema";
 import { and, eq } from "drizzle-orm";
 import { authMiddleware } from "@/server/hono/middleware";
-import { GoodResponse } from "@/shared/types";
+import { BadResponse, GoodResponse } from "@/shared/types";
+import { zValidator } from "@hono/zod-validator";
+import z from "zod";
+import { bookLibraryValues, bookLibraries } from "@/libs/shared/src/utils";
 
 export const userBooksApp = new Hono<Environment>();
 
@@ -22,3 +25,40 @@ userBooksApp.get("/:isbn", authMiddleware, async (c) => {
 
   return c.json(responseData);
 });
+
+userBooksApp.post(
+  "/:isbn/:library",
+  authMiddleware,
+  zValidator("param", z.object({ isbn: z.string(), library: z.enum(bookLibraryValues) }), (result, c) => {
+    if (!result.success) {
+      console.log(result.error);
+      const responseData: BadResponse = { success: false, errors: ["Invalid entries"] };
+      return c.json(responseData, 400);
+    }
+  }),
+  async (c) => {
+    const user = c.get("user");
+    const { isbn, library } = c.req.valid("param");
+
+    const existingUserBook = await db(c)
+      .select()
+      .from(userBooks)
+      .where(and(eq(userBooks.userId, user.id), eq(userBooks.isbn, isbn), eq(userBooks.libraryValue, library)));
+
+    if (existingUserBook.length === 0) {
+      await db(c).insert(userBooks).values({ isbn, libraryValue: library, userId: user.id });
+    }
+
+    await db(c)
+      .update(userBooks)
+      .set({ libraryValue: library })
+      .where(and(eq(userBooks.userId, user.id), eq(userBooks.isbn, isbn)));
+
+    const responseData: GoodResponse<string> = {
+      success: true,
+      data: `Added to ${bookLibraries.find((obj) => obj.value === library)?.name ?? "collection"}!`,
+    };
+
+    return c.json(responseData);
+  },
+);
