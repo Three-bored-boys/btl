7f7ef22c6f1af22784a1b1d30ff416e7c094125a
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-06 07:33:27 +0000

    refactor: book-page.ts hooks file

--- src/libs/client/src/hooks/book-page.ts
+++ src/libs/client/src/hooks/book-page.ts
@@ -1,30 +1,31 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "@/client/utils";
-import { SanitizedUser, UserBook } from "@/shared/db/schema";
-import { useAuthContext } from "./auth";
-import { useQuery } from "@tanstack/react-query";
+import { UserBook } from "@/shared/db/schema";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { BadResponse, GoodResponse } from "@/shared/types";
 
 const getUserBookWithISBN = async function (isbn: string) {
   try {
-    const { fetchDataResult, res } = await fetchData<{ user: SanitizedUser; book: UserBook[] }>(
+    const { fetchDataResult, res } = await fetchData<{ book: UserBook[] }>(
       `${process.env.NEXT_PUBLIC_API_URL}/user-books/${isbn}`,
       { credentials: "include", method: "GET" },
     );
 
     if (res.status === 401 || !fetchDataResult.success) {
-      return { user: null, libraryValue: null };
+      return { libraryValue: null };
     }
     const {
-      data: { user, book },
+      data: { book },
     } = fetchDataResult;
 
     if (book.length === 0) {
-      return { user, libraryValue: null };
+      return { libraryValue: null };
     }
 
-    return { user, libraryValue: book[0].libraryValue };
+    return { libraryValue: book[0].libraryValue };
   } catch (e) {
-    return { user: null, libraryValue: null };
+    return { libraryValue: null };
   }
 };
 
@@ -32,17 +33,69 @@ const useGetUserBookWithISBN = function (isbn: string) {
   return useQuery({ queryKey: ["user-books", isbn], queryFn: () => getUserBookWithISBN(isbn) });
 };
 
+const addUserBookWithISBN = async function (isbn: string, library: string) {
+  const { fetchDataResult, res } = await fetchData<string>(
+    `${process.env.NEXT_PUBLIC_API_URL}/user-books/${isbn}/${library}`,
+    {
+      credentials: "include",
+      method: "POST",
+    },
+  );
+
+  if (res.status === 401) {
+    throw new Error("User not authenticated");
+  }
+
+  return fetchDataResult;
+};
+
+const deleteUserBookWithISBN = async function (isbn: string) {
+  const { fetchDataResult, res } = await fetchData<string>(`${process.env.NEXT_PUBLIC_API_URL}/user-books/${isbn}`, {
+    credentials: "include",
+    method: "DELETE",
+  });
+
+  if (res.status === 401) {
+    throw new Error("User not authenticated");
+  }
+
+  return fetchDataResult;
+};
+
 export const useBookPage = function (isbn: string) {
+  const router = useRouter();
+  const queryClient = useQueryClient();
   const [libraryValue, setLibraryValue] = useState<string | null>(null);
-  const { setUser, user } = useAuthContext();
+  const [settledMessage, setSettledMessage] = useState<BadResponse | GoodResponse<string> | null>(null);
+
   const query = useGetUserBookWithISBN(isbn);
+
+  const mutation = useMutation({
+    mutationFn: (newLibraryValue: string | null) => {
+      if (newLibraryValue) {
+        return addUserBookWithISBN(isbn, newLibraryValue);
+      }
+
+      return deleteUserBookWithISBN(isbn);
+    },
+    onError: async () => {
+      await Promise.allSettled([
+        queryClient.invalidateQueries({ queryKey: ["btl_session_user"], exact: true, refetchType: "all" }),
+        queryClient.invalidateQueries({ queryKey: ["user-books", isbn], exact: true, refetchType: "all" }),
+      ]);
+      router.push("/login");
+    },
+    onSuccess: (data) => {
+      setSettledMessage(data);
+      router.refresh();
+    },
+  });
 
   useEffect(() => {
     if (query.data !== undefined) {
-      setUser(query.data.user);
       setLibraryValue(query.data.libraryValue);
     }
   }, [query.data]);
 
-  return { libraryValue, setLibraryValue, query, setUser, user };
+  return { libraryValue, setLibraryValue, query, mutation, settledMessage, setSettledMessage };
 };
