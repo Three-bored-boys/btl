6e37a3dbca6e8d708c6319650ded376aa7bf3e1e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-07 18:40:14 +0000

    refactor: move index hono init file to app '/api' route

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,47 @@
+import type { Environment } from "@/root/bindings";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { books } from "@/libs/server/src/hono/routes/books";
+import { auth } from "@/libs/server/src/hono/routes/auth";
+import { userBooksApp as userBooks } from "@/libs/server/src/hono/routes/userBooks";
+
+export const runtime = "nodejs";
+
+const app = new Hono<Environment>().basePath("/api");
+
+app.use(logger());
+app.use(
+  "*",
+  cors({
+    origin: (origin, _) => {
+      if (
+        origin.includes("-kosidinnas-projects.vercel.app") ||
+        origin === "https://b-t-l.vercel.app" ||
+        origin === "https://btl-nextjs-api-preview.kosiumeigbo.workers.dev" ||
+        origin === "https://btl-nextjs-api-prod.kosiumeigbo.workers.dev" ||
+        origin === "http://localhost:3000"
+      ) {
+        return origin;
+      } else {
+        return "http://localhost:8787";
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.get("/", (c) => {
+  return c.text("Welcome to the BTL API! 🚀");
+});
+
+app.route("/books", books);
+app.route("/auth", auth);
+app.route("/user-books", userBooks);
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const DELETE = handle(app);
+
+// export default app;
