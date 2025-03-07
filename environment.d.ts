a7663177702073d93061960bbff6b7288ec2baaf
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-07 16:19:48 +0000

    chore: update .env variables for hono api

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,11 @@
 declare namespace NodeJS {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface ProcessEnv {
-    readonly NEXT_PUBLIC_API_URL: string;
-    readonly API_URL: string;
     readonly URL: string;
     readonly NEXT_PUBLIC_URL: string;
+    readonly DATABASE_URL: string;
+    readonly GOOGLE_BOOKS_API_KEY: string;
+    readonly NY_TIMES_BOOKS_API_KEY: string;
+    readonly SESSION_SECRET_KEY: string;
   }
 }
