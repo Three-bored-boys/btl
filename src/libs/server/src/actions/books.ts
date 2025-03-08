1c5f8431df08e3a945c7734134e914960d39de12
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-08 23:08:11 +0000

    refactor: use server actions in genres-section component

--- src/libs/server/src/actions/books.ts
+++ src/libs/server/src/actions/books.ts
@@ -2,7 +2,9 @@
 
 import { unstable_cache } from "next/cache";
 import { NYTimesService } from "@/server/services/ny-times.service";
-import { BadResponse, BestSeller, GoodResponse } from "@/shared/types";
+import { GoogleBooksService } from "@/server/services/google.service";
+import { BadResponse, BestSeller, Book, GoodResponse } from "@/shared/types";
+import { z } from "zod";
 
 const getNYTBestSellers = async function () {
   const nytService = new NYTimesService(process.env.NY_TIMES_BOOKS_API_KEY!);
@@ -21,4 +23,29 @@ const getNYTBestSellers = async function () {
   return responseData;
 };
 
-export const getCachedNYTBestSellers = unstable_cache(getNYTBestSellers, [], { revalidate: 259200 });
+export const getCachedNYTBestSellers = unstable_cache(getNYTBestSellers, ["best-sellers"], { revalidate: 259200 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const getBooksByGenre = async function (genre: unknown) {
+  const validationResult = z.string().min(1).safeParse(genre);
+  if (!validationResult.success) {
+    const responseData: BadResponse = { success: false, errors: ["Invalid Input"], status: 404 };
+    return responseData;
+  }
+  const validGenre = validationResult.data;
+  const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY!);
+  const returnedValue = await googleBooksService.getBooksByAllParameters({
+    searchInput: { genre: validGenre },
+    paginationFilter: { maxResults: (6).toString() },
+  });
+
+  const responseData: GoodResponse<Book[]> = {
+    success: true,
+    data: returnedValue.books.filter((book) => book.isbn10 !== "" && book.isbn13 !== ""),
+  };
+
+  return responseData;
+};
+
+export const getCachedBooksByGenre = unstable_cache(getBooksByGenre, ["genres"], { revalidate: 259200 });
