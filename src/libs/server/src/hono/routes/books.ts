cbff191ff8b95714c94b0fff865b841a66ed9926
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-09 01:09:19 +0000

    chore: delete endpoint for latest-books in hono routes directory

--- src/libs/server/src/hono/routes/books.ts
+++ src/libs/server/src/hono/routes/books.ts
@@ -5,42 +5,29 @@ import { type BadResponse, type GoodResponse, /*  type Genres, type BestSeller, 
 import { fullSearchObjectSchema } from "@/shared/validators";
 import { Environment } from "@/root/bindings";
 
-export const books = new Hono<Environment>()
-  .get("/latest-books", async (c) => {
-    const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY!);
-    const books = await googleBooksService.getLatestBooks();
-
-    if (books.length === 0) {
-      const responseData: BadResponse = { success: false, errors: ["Trouble getting books"], status: 400 };
+export const books = new Hono<Environment>().get(
+  "/full-search",
+  zValidator("query", fullSearchObjectSchema, (result, c) => {
+    if (!result.success) {
+      console.log(result.error);
+      const responseData: BadResponse = { success: false, errors: ["Invalid entry"], status: 400 };
       return c.json(responseData, 400);
     }
+  }),
+  async (c) => {
+    const { page, maxResults, ...search } = c.req.valid("query");
+    const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY!);
 
-    const responseData: GoodResponse<Book[]> = { success: true, data: books };
+    const allBooksResults = await googleBooksService.getBooksByAllParameters({
+      searchInput: search,
+      paginationFilter: { maxResults, page },
+    });
+
+    const responseData: GoodResponse<{ books: Book[]; totalItems: number }> = {
+      success: true,
+      data: allBooksResults,
+    };
+
     return c.json(responseData);
-  })
-  .get(
-    "/full-search",
-    zValidator("query", fullSearchObjectSchema, (result, c) => {
-      if (!result.success) {
-        console.log(result.error);
-        const responseData: BadResponse = { success: false, errors: ["Invalid entry"], status: 400 };
-        return c.json(responseData, 400);
-      }
-    }),
-    async (c) => {
-      const { page, maxResults, ...search } = c.req.valid("query");
-      const googleBooksService = new GoogleBooksService(process.env.GOOGLE_BOOKS_API_KEY!);
-
-      const allBooksResults = await googleBooksService.getBooksByAllParameters({
-        searchInput: search,
-        paginationFilter: { maxResults, page },
-      });
-
-      const responseData: GoodResponse<{ books: Book[]; totalItems: number }> = {
-        success: true,
-        data: allBooksResults,
-      };
-
-      return c.json(responseData);
-    },
-  );
+  },
+);
