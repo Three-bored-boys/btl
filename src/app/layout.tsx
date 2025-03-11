4a95d0dd9e58edaa0c212fe1355e3fd87be8ff51
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-11 17:26:14 +0000

    refactor: edit AuthContextProvider and RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,23 +5,26 @@ import { ReactElement } from "react";
 import { ReactQueryProvider } from "@/client/providers/react-query-provider";
 import { RadixProvider } from "@/client/providers/radix-provider";
 import { AuthContextProvider } from "@/client/providers/auth-context-provider";
+import { getUserSession } from "@/server/auth/utils";
 
 export const metadata: Metadata = {
   title: "BTL (Book Tracker Library)",
   description: "A web application to view and store books in your library",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>): ReactElement {
+}>): Promise<ReactElement> {
+  const { user } = await getUserSession();
+
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body suppressHydrationWarning={true}>
         <ReactQueryProvider>
           <RadixProvider>
-            <AuthContextProvider>
+            <AuthContextProvider initialUser={user}>
               <main>{children}</main>
             </AuthContextProvider>
           </RadixProvider>
--- src/libs/client/src/providers/auth-context-provider.tsx
+++ src/libs/client/src/providers/auth-context-provider.tsx
@@ -3,42 +3,21 @@
 import { createContext } from "react";
 import { SanitizedUser } from "@/shared/db/schema";
 import React from "react";
-import { Spinner } from "@radix-ui/themes";
-import { Logo } from "@/client/components/ui/logo";
-import { useQuery } from "@tanstack/react-query";
-import { getUser } from "@/client/hooks/auth";
 
 export const AuthContext = createContext<{
   user: SanitizedUser | null;
   setUser: React.Dispatch<React.SetStateAction<SanitizedUser | null>>;
 } | null>(null);
 
-export const AuthContextProvider = function ({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = React.useState<SanitizedUser | null>(null);
-  const { data, isLoading } = useQuery({ queryKey: ["btl_session_user"], queryFn: () => getUser(), staleTime: 0 });
+export const AuthContextProvider = function ({
+  children,
+  initialUser,
+}: {
+  children: React.ReactNode;
+  initialUser: SanitizedUser | null;
+}) {
+  const [user, setUser] = React.useState<SanitizedUser | null>(initialUser);
   const value = React.useMemo(() => ({ user, setUser }), [user]);
-  React.useEffect(() => {
-    if (data !== undefined) {
-      setUser(data);
-    }
-  }, [data]);
-
-  if (isLoading) {
-    return <CheckingUserSession />;
-  }
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
-
-export const CheckingUserSession = function () {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="flex max-w-96 flex-col items-center justify-center gap-2 px-3">
-        <Spinner size={"3"} />
-        <div className="flex h-1 w-full items-center justify-center bg-primary"></div>
-        <Logo className="w-full text-7xl sm:text-9xl" />
-        <div className="flex h-1 w-full items-center justify-center bg-secondary"></div>
-      </div>
-    </div>
-  );
 };
