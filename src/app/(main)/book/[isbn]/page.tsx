ac47aa780e65de88a6824c0d775f07656e7c538f
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-11 14:46:39 +0000

    chore: minor changes

--- src/app/(main)/book/[isbn]/page.tsx
+++ src/app/(main)/book/[isbn]/page.tsx
@@ -1,5 +1,5 @@
-import { BookPageWrapper } from "@/root/src/libs/client/src/components/modules/book-page/book-page-wrapper";
+import { BookPage } from "@/root/src/libs/client/src/components/modules/book-page/book-page";
 
 export default function Book({ params: { isbn } }: { params: { isbn: string } }) {
-  return <BookPageWrapper params={{ isbn }} />;
+  return <BookPage params={{ isbn }} />;
 }
