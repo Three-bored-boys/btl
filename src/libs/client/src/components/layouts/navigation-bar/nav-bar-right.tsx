2f404955da14e9605d5a6c866cffb1a16ef33ce7
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-12 01:31:24 +0000

    refactor: update navbar

--- src/libs/client/src/components/layouts/navigation-bar/nav-bar-right.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/nav-bar-right.tsx
@@ -1,20 +1,18 @@
-"use client";
-
 import React, { ComponentProps } from "react";
 import type { NavAuthLinkArr } from "./nav-bar";
 import { LinkButton } from "@/client/components/ui/link-button";
 import { cn } from "@/client/utils";
-import { useAuthContext } from "@/client/hooks/auth";
 import { Avatar, DropdownMenu } from "@radix-ui/themes";
 import avatarImage from "@/public/assets/images/avatar.png";
 import { Logout } from "@/client/components/modules/auth/logout/logout";
+import { getUserSession } from "@/root/src/libs/server/src/auth/utils";
 
 type NavBarRightProps = {
   routesArr: NavAuthLinkArr;
 } & ComponentProps<"div">;
 
-export function NavBarRight({ routesArr, className }: NavBarRightProps): React.ReactElement {
-  const { user } = useAuthContext();
+export async function NavBarRight({ routesArr, className }: NavBarRightProps): Promise<React.ReactElement> {
+  const { user } = await getUserSession();
 
   return (
     <div className="flex items-center">
--- src/libs/client/src/components/layouts/navigation-bar/nav-links-main.tsx
+++ src/libs/client/src/components/layouts/navigation-bar/nav-links-main.tsx
@@ -1,15 +1,13 @@
-"use client";
-
-import { NavLinks } from "./nav-links";
-import { useRootPathnameContext } from "@/client/hooks/root-pathname";
 import { type NavLinkArr } from "./nav-bar";
+import { getUserSession } from "@/root/src/libs/server/src/auth/utils";
+import { NavLinks } from "./nav-links";
 
-export const NavLinksMain = function ({ navLinkArr }: { navLinkArr: NavLinkArr }) {
-  const { rootPathname } = useRootPathnameContext();
+export const NavLinksMain = async function ({ navLinkArr }: { navLinkArr: NavLinkArr }) {
+  const { user } = await getUserSession();
 
   return (
     <div className="hidden md:flex md:items-center">
-      <NavLinks device={"no-mobile"} routesArr={navLinkArr} rootPathname={rootPathname} className="w-full" />
+      <NavLinks device={"no-mobile"} routesArr={navLinkArr} className="w-full" user={user} />
     </div>
   );
 };
