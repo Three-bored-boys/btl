19d92fae2b177cae675ee3c6b1245bbac91e0b82
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-14 11:29:51 +0000

    chore: edit redirects

--- src/libs/server/src/actions/userBooks.ts
+++ src/libs/server/src/actions/userBooks.ts
@@ -29,19 +29,18 @@ export const getCachedUserBookLibraryValue = unstable_cache(getUserBookLibraryVa
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-export const addUserBook = async function (
-  {
-    isbn,
-    library,
-  }: {
-    isbn: unknown;
-    library: unknown;
-  },
-  pathname: string,
-): Promise<BadResponse | GoodResponse<string>> {
+export const addUserBook = async function ({
+  isbn,
+  library,
+  redirectUrl,
+}: {
+  isbn: unknown;
+  library: unknown;
+  redirectUrl: string;
+}): Promise<BadResponse | GoodResponse<string>> {
   const { user } = await getUserSession();
   if (!user) {
-    redirect(`/login?redirect=${encodeURIComponent(pathname)}`);
+    redirect(`/login?redirect=${encodeURIComponent(redirectUrl)}`);
   }
 
   const validation = z.object({ isbn: z.string(), library: z.enum(bookLibraryValues) }).safeParse({ isbn, library });
@@ -88,10 +87,10 @@ export const addUserBook = async function (
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-export const deleteUserBook = async function ({ isbn }: { isbn: string }, pathname: string) {
+export const deleteUserBook = async function ({ isbn, redirectUrl }: { isbn: string; redirectUrl: string }) {
   const { user } = await getUserSession();
   if (!user) {
-    redirect(`/login?redirect=${encodeURIComponent(pathname)}`);
+    redirect(`/login?redirect=${encodeURIComponent(redirectUrl)}`);
   }
 
   try {
