3b289d7201a8a103926ea31594df60fd6c38be31
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-15 09:10:44 +0000

    chore: edit AuthLinkButton component

--- src/libs/client/src/components/ui/auth-link-button.tsx
+++ src/libs/client/src/components/ui/auth-link-button.tsx
@@ -6,10 +6,18 @@ import { LinkButtonProps, LinkButton } from "./link-button";
 
 export function AuthLinkButton(authLinkButtonProps: LinkButtonProps): React.ReactElement {
   const redirectUrl = useWindowLocationHref();
-  const { href: authLink, children, ...props } = authLinkButtonProps;
-  const newHref = `${authLink}?redirect=${encodeURIComponent(redirectUrl)}`;
+  const { href, children, ...props } = authLinkButtonProps;
+  const newHref = `${href}?redirect=${encodeURIComponent(redirectUrl)}`;
+  if (href === "/login" || href === "/signup") {
+    return (
+      <LinkButton href={newHref} {...props}>
+        {children}
+      </LinkButton>
+    );
+  }
+
   return (
-    <LinkButton href={newHref} {...props}>
+    <LinkButton href={href} {...props}>
       {children}
     </LinkButton>
   );
