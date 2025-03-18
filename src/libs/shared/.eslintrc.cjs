9a24afaed8bf81b2e57722d8b388985b5143570a
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-18 23:20:42 +0000

    build: update eslint to 9.x.x and tseslint to 8.x.x

--- src/libs/shared/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  "extends": [path.join(process.cwd(), "/.eslintrc.json")],
-  "parserOptions": { "project": true, "tsconfigRootDir": __dirname },
-};
