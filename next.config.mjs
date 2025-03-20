340bea9db746758a97e68eff331cafab3a48942e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-20 23:57:44 +0000

    build: update next config to allow images from Open Library

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "books.google.com" }, { hostname: "storage.googleapis.com" }],
+    remotePatterns: [
+      { hostname: "books.google.com" },
+      { hostname: "storage.googleapis.com" },
+      { hostname: "covers.openlibrary.org" },
+    ],
   },
   eslint: { dirs: ["src/app", "src/libs"] },
 };
