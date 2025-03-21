0fa2a42d16d5e9984f286fb559789654168184be
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-21 12:43:03 +0000

    chore: use postgres-js instead of neon

--- src/libs/server/src/db/migrate.ts
+++ src/libs/server/src/db/migrate.ts
@@ -1,16 +1,10 @@
-import { config } from "dotenv";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-config({ path: ".env" });
-
 const main = async () => {
   try {
     const connectionString = process.env.DATABASE_URL;
-    if (!connectionString) {
-      throw Error("Database URL not provided");
-    }
     const migrationClient = drizzle(postgres(connectionString, { max: 1 }));
     await migrate(migrationClient, { migrationsFolder: "drizzle" });
     console.log("Migration complete");
--- src/libs/server/src/db/seed.ts
+++ src/libs/server/src/db/seed.ts
@@ -1,23 +1,14 @@
 import { users } from "./schema";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-import { config } from "dotenv";
 
-config({ path: ".dev.vars" });
+const connectionString = process.env.DATABASE_URL;
 
-const db = function () {
-  const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) {
-    throw Error("Database URL not provided");
-  }
-  return drizzle(postgres(connectionString));
-};
+const db = drizzle(postgres(connectionString));
 
 const main = async function () {
   try {
-    await db()
-      .insert(users)
-      .values([{ emailAddress: "goat@goat.com", userName: "goat", hashedPassword: "goat" }]);
+    await db.insert(users).values([{ emailAddress: "goat@goat.com", userName: "goat", hashedPassword: "goat" }]);
     console.log("Seed complete");
   } catch (e) {
     console.log(e);
