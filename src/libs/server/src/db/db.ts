195256fb0c6755483e0c8ba008982af422546ccb
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-29 19:45:46 +0000

    chore: update db.ts with block comment for future reference

--- src/libs/server/src/db/db.ts
+++ src/libs/server/src/db/db.ts
@@ -1,3 +1,16 @@
+/* import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+const createClient = function () {
+  const connectionString = process.env.DATABASE_URL;
+
+  const client = neon(connectionString, { fetchOptions: { timeout: 30000 } });
+  return client;
+};
+
+export const db = drizzle(createClient());
+ */
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
