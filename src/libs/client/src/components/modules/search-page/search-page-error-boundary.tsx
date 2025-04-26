09fbe80ce0e79f1ebc82a8b655eba9c696356555
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-26 18:08:44 +0100

    chore: remove unnecessary Container component

--- src/libs/client/src/components/modules/search-page/search-page-error-boundary.tsx
+++ src/libs/client/src/components/modules/search-page/search-page-error-boundary.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Container } from "@/libs/client/src/components/layouts/container";
 import { Button } from "@/libs/client/src/components/ui/button";
 import Image from "next/image";
 import notFoundImage from "@/public/assets/images/not-found.webp";
@@ -15,26 +14,24 @@ export function SearchPageErrorBoundary({
 }) {
   return (
     <div className="relative min-h-screen w-full">
-      <Container>
-        <div className="flex flex-col items-center justify-start gap-y-3 py-5">
-          <ExclamationTriangle></ExclamationTriangle>
-          <h2 className="mb-2 text-4xl radix-xs:text-5xl md:mb-5 md:text-7xl">
-            Something has gone wrong with this request
-          </h2>
-          <p className="mb-8 text-base radix-xs:text-xl md:mb-3 md:text-2xl">{error.message}</p>
-          <Button
-            background={"light"}
-            textSize={"big"}
-            className="mb-28 radix-xs:mb-9"
-            onClick={() => resetErrorBoundary()}
-          >
-            Try again
-          </Button>
-          <div>
-            <Image src={notFoundImage} alt="Cartoon image of man sitting on floor and reading a book"></Image>
-          </div>
+      <div className="flex flex-col items-center justify-start gap-y-3 py-5">
+        <ExclamationTriangle></ExclamationTriangle>
+        <h2 className="mb-2 text-4xl radix-xs:text-5xl md:mb-5 md:text-7xl">
+          Something has gone wrong with this request
+        </h2>
+        <p className="mb-8 text-base radix-xs:text-xl md:mb-3 md:text-2xl">{error.message}</p>
+        <Button
+          background={"light"}
+          textSize={"big"}
+          className="mb-28 radix-xs:mb-9"
+          onClick={() => resetErrorBoundary()}
+        >
+          Try again
+        </Button>
+        <div>
+          <Image src={notFoundImage} alt="Cartoon image of man sitting on floor and reading a book"></Image>
         </div>
-      </Container>
+      </div>
     </div>
   );
 }
