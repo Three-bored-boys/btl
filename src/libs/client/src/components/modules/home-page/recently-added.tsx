fad09d4a9af2525212f31a56e17ad48365199826
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-28 22:59:13 +0100

    chore: have to commit

--- src/libs/client/src/components/modules/home-page/recently-added.tsx
+++ src/libs/client/src/components/modules/home-page/recently-added.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Container } from "@/client/components/layouts/container";
 
 export async function RecentlyAdded({ user }: { user: SanitizedUser }) {
+  console.log("Currently in the RecentlyAdded component");
+
   return (
     <section className="w-full py-10">
       <Container>Recently added</Container>
