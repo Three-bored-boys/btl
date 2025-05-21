27f8d1e8cae1804092a9ce9d94e835113ede274d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-05-21 23:46:51 +0100

    feat: create layout component for RecentlyAdded

--- src/libs/client/src/components/modules/home-page/recently-added.tsx
+++ src/libs/client/src/components/modules/home-page/recently-added.tsx
@@ -1,14 +1,22 @@
 import { SanitizedUser } from "@/root/src/libs/shared/src/db/schema";
 import React from "react";
 import { Container } from "@/client/components/layouts/container";
+import { ExclamationTriangle } from "@/client/components/ui/icons/exclamation-triangle";
+import { getRecentlyAddedBooks } from "@/server/actions";
+import { BookCard } from "./book-card";
+import { SectionPreamble } from "./section-preamble";
 
-export async function RecentlyAdded({ user }: { user: SanitizedUser }) {
-  console.log("Here in Recently Added.");
-  console.log("Here in Recently Added.");
+export async function RecentlyAdded({ user }: { user: SanitizedUser }) {}
 
+const RecentlyAddedLayout = function ({ children }: { children: React.ReactNode }) {
   return (
-    <section className="w-full py-10">
-      <Container>Recently added</Container>
+    <section className="mb-20 w-full py-10">
+      <Container>
+        <SectionPreamble title="Recently Added Books To Collection">
+          A brief overview of the latest books added to your collection
+        </SectionPreamble>
+        {children}
+      </Container>
     </section>
   );
-}
+};
