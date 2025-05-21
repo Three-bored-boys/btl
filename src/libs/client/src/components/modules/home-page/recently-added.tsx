b967b51efc24d7162205c223799248f3bb0a0bd7
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-05-21 23:49:18 +0100

    style: edit RecentlyAdded component

--- src/libs/client/src/components/modules/home-page/recently-added.tsx
+++ src/libs/client/src/components/modules/home-page/recently-added.tsx
@@ -6,7 +6,43 @@ import { getRecentlyAddedBooks } from "@/server/actions";
 import { BookCard } from "./book-card";
 import { SectionPreamble } from "./section-preamble";
 
-export async function RecentlyAdded({ user }: { user: SanitizedUser }) {}
+export async function RecentlyAdded({ user }: { user: SanitizedUser }) {
+  const result = await getRecentlyAddedBooks({ user });
+  if (!result.success) {
+    const { errors, status } = result;
+    return (
+      <RecentlyAddedLayout>
+        <div className="my-2 flex w-full flex-col items-center justify-start gap-y-1">
+          <ExclamationTriangle />
+          <p className="text-xl font-semibold">Error {status}</p>
+          <p className="text-base font-normal">{errors[0]}</p>
+        </div>
+      </RecentlyAddedLayout>
+    );
+  }
+
+  const { data } = result;
+  if (data.length === 0) {
+    return (
+      <RecentlyAddedLayout>
+        <div>No books in recently added</div>
+      </RecentlyAddedLayout>
+    );
+  }
+
+  return (
+    <RecentlyAddedLayout>
+      <div className="grid grid-cols-5 gap-10">
+        {data.map((obj, i) => (
+          <div key={i}>
+            <BookCard book={obj.book}></BookCard>
+            <div className="mt-4 text-wrap text-sm italic">Added to collection at {obj.date.toString()}</div>
+          </div>
+        ))}
+      </div>
+    </RecentlyAddedLayout>
+  );
+}
 
 const RecentlyAddedLayout = function ({ children }: { children: React.ReactNode }) {
   return (
