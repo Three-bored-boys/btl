8f357dd465ac0ae74b63d14b91d5c97be2c5d6f5
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-05-26 23:30:02 +0100

    chore: style recently added section

--- src/libs/client/src/components/modules/home-page/recently-added.tsx
+++ src/libs/client/src/components/modules/home-page/recently-added.tsx
@@ -3,8 +3,11 @@ import React from "react";
 import { Container } from "@/client/components/layouts/container";
 import { ExclamationTriangle } from "@/client/components/ui/icons/exclamation-triangle";
 import { getRecentlyAddedBooks } from "@/server/actions";
-import { BookCard } from "./book-card";
 import { SectionPreamble } from "./section-preamble";
+import Link from "next/link";
+import Image from "next/image";
+
+const genericBookImage = "/assets/images/generic-book.png";
 
 export async function RecentlyAdded({ user }: { user: SanitizedUser }) {
   const result = await getRecentlyAddedBooks({ user });
@@ -32,11 +35,21 @@ export async function RecentlyAdded({ user }: { user: SanitizedUser }) {
 
   return (
     <RecentlyAddedLayout>
-      <div className="grid grid-cols-5 gap-10">
-        {data.map((obj, i) => (
-          <div key={i}>
-            <BookCard book={obj.book}></BookCard>
-            <div className="mt-4 text-wrap text-sm italic">Added to collection at {obj.date.toString()}</div>
+      <div className="flex w-full items-start justify-between gap-10 overflow-x-auto">
+        {data.map(({ book, date }, i) => (
+          <div key={i} className="min-w-40 max-w-40">
+            <Link href={`/book/${book.isbn13 ? book.isbn13 : book.isbn10}`} className="w-full">
+              <Image
+                src={book.image ? book.image : genericBookImage}
+                alt={`${book.title} by ${book.author}`}
+                className="h-60 w-full rounded-lg object-cover"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <h4 className="truncate font-semibold">{book.title}</h4>
+            <p className="truncate">{book.author}</p>
+            <p className="mt-4 text-sm italic">Added to collection at {date.toString()}</p>
           </div>
         ))}
       </div>
