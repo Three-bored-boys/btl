12d394c3e64418494f92a429e65430a52389f5b4
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-05-29 14:01:43 +0100

    test: add quote.tsx test file to play around

---
+++ src/libs/client/src/components/modules/home-page/quotes-section/quote.test.tsx
@@ -0,0 +1,9 @@
+import { Quote } from "./quote";
+import { render, screen } from "@testing-library/react";
+
+test("Main Test", () => {
+  render(<Quote author="Michael Gallaway" quote="He who conquers"></Quote>);
+  const quoteEl = screen.getByText(/He who/i);
+  console.log(quoteEl.innerHTML);
+  expect(quoteEl).toBeVisible();
+});
