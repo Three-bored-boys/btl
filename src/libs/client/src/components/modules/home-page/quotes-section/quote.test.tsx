c03556a1585035033c2673e89ca53ab7a24ee08b
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-05-29 18:52:01 +0100

    chore: edit test file

--- src/libs/client/src/components/modules/home-page/quotes-section/quote.test.tsx
+++ src/libs/client/src/components/modules/home-page/quotes-section/quote.test.tsx
@@ -4,6 +4,6 @@ import { render, screen } from "@testing-library/react";
 test("Main Test", () => {
   render(<Quote author="Michael Gallaway" quote="He who conquers"></Quote>);
   const quoteEl = screen.getByText(/He who/i);
-  console.log(quoteEl.outerHTML);
+  screen.debug(quoteEl);
   expect(quoteEl).toBeVisible();
 });
