344cd62b71c0fa87bc1f759e4c9eeead3ae83441
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-08-25 01:19:28 +0100

    chore: edit seed.ts to run the 'books' table population

--- src/libs/server/src/db/seed.ts
+++ src/libs/server/src/db/seed.ts
@@ -1,7 +1,8 @@
-import { userBooks } from "./schema";
+import { userBooks, books } from "./schema";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import "dotenv/config";
+import { GoogleBooksService } from "@/server/services/google.service";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -11,8 +12,36 @@ const main = async function () {
   try {
     console.log(connectionString);
     const allUserBooks = await db.select({ isbn: userBooks.isbn }).from(userBooks);
-    console.log(allUserBooks.map((obj) => obj.isbn));
-    console.log("Seed complete");
+    const allUserBooksIsbn = allUserBooks.map((obj) => obj.isbn);
+    console.log("all the isbn's: ", allUserBooksIsbn);
+
+    const googleBooksAPIKey = process.env.GOOGLE_BOOKS_API_KEY;
+    const googleBooksService = new GoogleBooksService(googleBooksAPIKey);
+
+    const settledInsertPromises = await Promise.allSettled(
+      allUserBooksIsbn.map(async (isbn, i) => {
+        const bookArray = await googleBooksService.getBookByISBN(isbn);
+
+        if (bookArray.length === 0) {
+          console.log(`Book with ISBN ${isbn} cannot be found`);
+          throw new Error(`Book with ISBN ${isbn} cannot be found`);
+        }
+
+        const [book] = bookArray;
+
+        await db
+          .insert(books)
+          .values(book)
+          .onConflictDoUpdate({ target: [books.isbn13, books.isbn10], set: book });
+        console.log("Book now inserted! ", book);
+
+        console.log(`Seed for ${book.title ?? book.isbn13 ?? book.isbn10 ?? i.toString()} now complete`);
+
+        return 0;
+      }),
+    );
+
+    console.log(settledInsertPromises);
   } catch (e) {
     console.log(e);
   }
